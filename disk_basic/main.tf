d74173b3057b42a06b7491b19d1597115c0fb5f9
Riley Karson <rileykarson@google.com>
2018-10-16 15:04:44 -0700

    Add OiCS Examples

---
+++ disk_basic/main.tf
@@ -0,0 +1,9 @@
+resource "google_compute_disk" "default" {
+  name  = "test-disk-${local.name_suffix}"
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  image = "debian-8-jessie-v20170523"
+  labels {
+    environment = "dev"
+  }
+}
