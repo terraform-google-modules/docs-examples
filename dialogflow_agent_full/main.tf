6edf3b8eecf68da48d99bd6e5ae3dfb9997e51f5
The Magician <magic-modules@google.com>
2020-01-31 12:57:22 -0800

    Add Dialogflow Agent resource (#3014) (#8)
    * Dialogflow Agent support
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dialogflow_agent_full/main.tf
@@ -0,0 +1,13 @@
+resource "google_dialogflow_agent" "full_agent" {
+  display_name = "tf-test-full-agent-${local.name_suffix}"
+  default_language_code = "en"
+  supported_language_codes = ["fr","de","es"]
+  time_zone = "America/New_York"
+  description = "Example description."
+  avatar_uri = "https://cloud.google.com/_static/images/cloud/icons/favicons/onecloud/super_cloud.png"
+  enable_logging = true
+  match_mode = "MATCH_MODE_ML_ONLY"
+  classification_threshold = 0.3
+  api_version = "API_VERSION_V2_BETA_1"
+  tier = "TIER_STANDARD"
+}
