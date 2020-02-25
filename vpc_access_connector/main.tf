d090e10f539363be15a90e7748753963c43d2a80
The Magician <magic-modules@google.com>
2020-02-24 17:33:49 -0800

    VPC serverless access update version from beta to GA (#3053) (#15)
    * remove beta provider
    
    vpc access connector is now GA
    https://cloud.google.com/functions/docs/release-notes#December_11_2019
    
    * updated versions for VPC serverless acess
    
    * add back beta version
    
    * remove beta references in resource
    
    * remove google-beta provider from example
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- vpc_access_connector/main.tf
+++ vpc_access_connector/main.tf
@@ -1,9 +1,5 @@
-provider "google-beta" {
-}
-
 resource "google_vpc_access_connector" "connector" {
-  name          = "my-connector-${local.name_suffix}"
-  provider      = google-beta
+  name          = "vpcconn-${local.name_suffix}"
   region        = "us-central1"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
