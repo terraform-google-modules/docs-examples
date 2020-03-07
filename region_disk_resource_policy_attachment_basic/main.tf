b34eda364f958c0737e1a856c080423655aace77
The Magician <magic-modules@google.com>
2020-03-06 17:06:16 -0800

    Regional disk resource policies attachment resource. (#3228) (#19)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_disk_resource_policy_attachment_basic/main.tf
@@ -0,0 +1,46 @@
+resource "google_compute_region_disk_resource_policy_attachment" "attachment" {
+  name = google_compute_resource_policy.policy.name
+  disk = google_compute_region_disk.ssd.name
+  region = "us-central1"
+}
+
+resource "google_compute_disk" "disk" {
+  name  = "my-base-disk-${local.name_suffix}"
+  image = "debian-cloud/debian-9"
+  size  = 50
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+resource "google_compute_snapshot" "snapdisk" {
+  name  = "my-snapshot-${local.name_suffix}"
+  source_disk = google_compute_disk.disk.name
+  zone        = "us-central1-a"
+}
+
+resource "google_compute_region_disk" "ssd" {
+  name  = "my-disk-${local.name_suffix}"
+  replica_zones = ["us-central1-a", "us-central1-f"]
+  snapshot = google_compute_snapshot.snapdisk.self_link
+  size  = 50
+  type  = "pd-ssd"
+  region  = "us-central1"
+}
+
+resource "google_compute_resource_policy" "policy" {
+  name = "my-resource-policy-${local.name_suffix}"
+  region = "us-central1"
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time = "04:00"
+      }
+    }
+  }
+}
+
+data "google_compute_image" "my_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
