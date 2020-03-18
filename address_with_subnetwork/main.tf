44eff7b7981c4f5925dac1df071310f9cb90ceec
The Magician <magic-modules@google.com>
2020-03-17 18:59:47 -0700

    fixed ids for #5542 (#3205) (#25)
    * fixed ids for #5542
    
    * fixed ids for #5542 and replacing self_links
    
    * fixed ids for #5542
    
    * Update datasource_google_compute_backend_service.html.markdown
    
    (forcing update to build)
    
    * Updated id value
    
    Co-authored-by: edward <sunedward@google.com>
    Co-authored-by: emily <emilyye@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: edward <sunedward@google.com>
    Co-authored-by: emily <emilyye@google.com>

--- address_with_subnetwork/main.tf
+++ address_with_subnetwork/main.tf
@@ -6,12 +6,12 @@ resource "google_compute_subnetwork" "default" {
   name          = "my-subnet-${local.name_suffix}"
   ip_cidr_range = "10.0.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.default.self_link
+  network       = google_compute_network.default.id
 }
 
 resource "google_compute_address" "internal_with_subnet_and_address" {
   name         = "my-internal-address-${local.name_suffix}"
-  subnetwork   = google_compute_subnetwork.default.self_link
+  subnetwork   = google_compute_subnetwork.default.id
   address_type = "INTERNAL"
   address      = "10.0.42.42"
   region       = "us-central1"
