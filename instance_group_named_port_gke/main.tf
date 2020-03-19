a7df05df0fb4e4f52f58837618ad601aca9a5d72
Modular Magician <magic-modules@google.com>
2020-03-19 15:06:14 +0000

    Add a resource for fine-grained management of named ports (#3236)
    * Add a resource for fine-grained management of named ports in instance groups
    
    * Add excludes
    
    * second ansible exclude
    
    * Add back delete
    
    * Fix schema, docs
    
    * Add mutex, pull extra values from group
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ instance_group_named_port_gke/main.tf
@@ -0,0 +1,41 @@
+resource "google_compute_instance_group_named_port" "my_port" {
+  group = google_container_cluster.my_cluster.instance_group_urls[0]
+  zone = "us-central1-a"
+
+  name = "http"
+  port = 8080
+}
+
+resource "google_compute_instance_group_named_port" "my_ports" {
+  group = google_container_cluster.my_cluster.instance_group_urls[0]
+  zone = "us-central1-a"
+
+  name = "https"
+  port = 4443
+}
+
+resource "google_compute_network" "container_network" {
+  name                    = "container-network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "container_subnetwork" {
+  name                     = "container-subnetwork-${local.name_suffix}"
+  region                   = "us-central1"
+  network                  = google_compute_network.container_network.name
+  ip_cidr_range            = "10.0.36.0/24"
+}
+
+resource "google_container_cluster" "my_cluster" {
+  name               = "my-cluster-${local.name_suffix}"
+  location           = "us-central1-a"
+  initial_node_count = 1
+
+  network    = google_compute_network.container_network.name
+  subnetwork = google_compute_subnetwork.container_subnetwork.name
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "/19"
+    services_ipv4_cidr_block = "/22"
+  }
+}
