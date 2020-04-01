1a360df3cccc66c2b87183f59c6b7bea62ac75b6
Modular Magician <magic-modules@google.com>
2020-04-01 21:45:26 +0000

    init os-login sshkey (#3221)
    * init os-login sshkey
    
    Signed-off-by: Sébastien GLON <sebglon@gmail.com>
    
    * Fix naming + example
    
    * Fix endpoint
    
    * clean build folder
    
    * Apply suggestions from code review
    
    Co-Authored-By: Riley Karson <rileykarson@google.com>
    
    * Fix review
    
    * Add post_create
    
    * Add reference doc
    
    * Try to put user on url request
    
    * Fix user
    
    * Get the default userinfo email
    
    * Update products/oslogin/api.yaml
    
    Co-Authored-By: Riley Karson <rileykarson@google.com>
    
    * Apply suggestions from code review
    
    * revert url_param_only
    
    * creation fixed
    
    * Work except import
    
    * Add id_format and import_format
    
    * Add Id
    
    * Fix id
    
    * Update source_repo_repository_update.go.erb
    
    * Update source_repo_repository_update.go.erb
    
    * revert
    
    * Apply suggestions from code review
    
    Co-Authored-By: Riley Karson <rileykarson@google.com>
    
    * Add sidebar link
    
    Co-authored-by: Sébastien GLON <sebglon@gmail.com>
    Co-authored-by: Riley Karson <rileykarson@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ os_login_ssh_key_provided_user/main.tf
@@ -0,0 +1,7 @@
+data "google_client_openid_userinfo" "me" {
+}
+
+resource "google_os_login_ssh_public_key" "cache" {
+  user =  data.google_client_openid_userinfo.me.email
+  key = file("../static/ssh_rsa.pub")
+}
