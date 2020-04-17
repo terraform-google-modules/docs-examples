23f658c88394077d9a721c01c7c4013a23092ab6
The Magician <magic-modules@google.com>
2020-04-17 08:16:32 -0700

    adding bigquery job (#3331) (#34)
    * adding bigquery job
    
    * remove dry run option
    
    * exclude from inspec and ansible
    
    * remove table reference
    
    * update tests to make them run
    
    * code review comment changes
    
    * change back to BigQuery
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_job_query/main.tf
@@ -0,0 +1,36 @@
+resource "google_bigquery_table" "foo" {
+  dataset_id = google_bigquery_dataset.bar.dataset_id
+  table_id   = "job_query-${local.name_suffix}_table"
+}
+
+resource "google_bigquery_dataset" "bar" {
+  dataset_id                  = "job_query-${local.name_suffix}_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+}
+
+resource "google_bigquery_job" "job" {
+  job_id     = "job_query-${local.name_suffix}"
+
+  labels = {
+    "example-label" ="example-value"
+  }
+
+  query {
+    query = "SELECT state FROM [lookerdata:cdc.project_tycho_reports]"
+
+    destination_table {
+      project_id = google_bigquery_table.foo.project
+      dataset_id = google_bigquery_table.foo.dataset_id
+      table_id   = google_bigquery_table.foo.table_id
+    }
+
+    allow_large_results = true
+    flatten_results = true
+
+    script_options {
+      key_result_statement = "LAST"
+    }
+  }
+}
