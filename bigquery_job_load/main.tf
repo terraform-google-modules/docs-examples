a5e7897b342a6ed4f28af616c8d16ec6c8987dbe
Modular Magician <magic-modules@google.com>
2020-04-17 15:16:19 +0000

    adding bigquery job (#3331)
    * adding bigquery job
    
    * remove dry run option
    
    * exclude from inspec and ansible
    
    * remove table reference
    
    * update tests to make them run
    
    * code review comment changes
    
    * change back to BigQuery
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_job_load/main.tf
@@ -0,0 +1,37 @@
+resource "google_bigquery_table" "foo" {
+  dataset_id = google_bigquery_dataset.bar.dataset_id
+  table_id   = "job_load-${local.name_suffix}_table"
+}
+
+resource "google_bigquery_dataset" "bar" {
+  dataset_id                  = "job_load-${local.name_suffix}_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+}
+
+resource "google_bigquery_job" "job" {
+  job_id     = "job_load-${local.name_suffix}"
+
+  labels = {
+    "my_job" ="load"
+  }
+
+  load {
+    source_uris = [
+      "gs://cloud-samples-data/bigquery/us-states/us-states-by-date.csv",
+    ]
+
+    destination_table {
+      project_id = google_bigquery_table.foo.project
+      dataset_id = google_bigquery_table.foo.dataset_id
+      table_id   = google_bigquery_table.foo.table_id
+    }
+
+    skip_leading_rows = 1
+    schema_update_options = ["ALLOW_FIELD_RELAXATION", "ALLOW_FIELD_ADDITION"]
+
+    write_disposition = "WRITE_APPEND"
+    autodetect = true
+  }
+}
