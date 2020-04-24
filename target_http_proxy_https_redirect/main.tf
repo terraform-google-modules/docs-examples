88e5120e4da4bb83eac766e76f4b3332319d8c92
Modular Magician <magic-modules@google.com>
2020-04-24 22:45:15 +0000

    Add default url redirect to url maps (#3379)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ target_http_proxy_https_redirect/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_target_http_proxy" "default" {
+  name    = "test-https-redirect-proxy-${local.name_suffix}"
+  url_map = google_compute_url_map.default.self_link
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "url-map-${local.name_suffix}"
+  default_url_redirect {
+    https_redirect = true
+  }
+}
