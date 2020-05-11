0a62975bdccb4a04a74db3ce3fde4a1b86e40b76
Modular Magician <magic-modules@google.com>
2020-05-11 21:15:39 +0000

    Replace self_link (part 2 of #5542) (#3478)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- network_peering_routes_config_basic/main.tf
+++ network_peering_routes_config_basic/main.tf
@@ -8,8 +8,8 @@ resource "google_compute_network_peering_routes_config" "peering_primary_routes"
 
 resource "google_compute_network_peering" "peering_primary" {
   name         = "primary-peering-${local.name_suffix}"
-  network      = google_compute_network.network_primary.self_link
-  peer_network = google_compute_network.network_secondary.self_link
+  network      = google_compute_network.network_primary.id
+  peer_network = google_compute_network.network_secondary.id
 
   import_custom_routes = true
   export_custom_routes = true
@@ -17,8 +17,8 @@ resource "google_compute_network_peering" "peering_primary" {
 
 resource "google_compute_network_peering" "peering_secondary" {
   name         = "secondary-peering-${local.name_suffix}"
-  network      = google_compute_network.network_secondary.self_link
-  peer_network = google_compute_network.network_primary.self_link
+  network      = google_compute_network.network_secondary.id
+  peer_network = google_compute_network.network_primary.id
 }
 
 resource "google_compute_network" "network_primary" {
