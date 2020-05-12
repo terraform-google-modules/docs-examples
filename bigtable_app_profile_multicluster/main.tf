0dc6668b8f554b91e11b5e632d25952a6eccecdd
The Magician <magic-modules@google.com>
2020-05-12 17:57:43 -0400

    add deletion_protection field for bigtable instance (#3450) (#45)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- bigtable_app_profile_multicluster/main.tf
+++ bigtable_app_profile_multicluster/main.tf
@@ -6,6 +6,8 @@ resource "google_bigtable_instance" "instance" {
     num_nodes    = 3
     storage_type = "HDD"
   }
+
+  deletion_protection  = "false"
 }
 
 resource "google_bigtable_app_profile" "ap" {
