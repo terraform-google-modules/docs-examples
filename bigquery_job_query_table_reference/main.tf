b4f83da315f212c9adfa7a9ccd7c9f06b5866ff9
The Magician <magic-modules@google.com>
2020-05-18 12:47:28 -0400

    allow bigquery job fields that reference resources in blocks to do so with one field (#3519) (#47)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_job_query_table_reference/main.tf
@@ -0,0 +1,38 @@
+resource "google_bigquery_table" "foo" {
+  dataset_id = google_bigquery_dataset.bar.dataset_id
+  table_id   = "job_query-${local.name_suffix}_table"
+}
+
+resource "google_bigquery_dataset" "bar" {
+  dataset_id                  = "job_query-${local.name_suffix}_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "US"
+}
+
+resource "google_bigquery_job" "job" {
+  job_id     = "job_query-${local.name_suffix}"
+
+  labels = {
+    "example-label" ="example-value"
+  }
+
+  query {
+    query = "SELECT state FROM [lookerdata:cdc.project_tycho_reports]"
+
+    destination_table {
+      table_id = google_bigquery_table.foo.id
+    }
+
+    default_dataset {
+      dataset_id = google_bigquery_dataset.bar.id
+    }
+
+    allow_large_results = true
+    flatten_results = true
+
+    script_options {
+      key_result_statement = "LAST"
+    }
+  }
+}
