184f7aeeae8e45bfa32ac0fe16722a725266f5dc
The Magician <magic-modules@google.com>
2020-05-20 15:24:23 -0400

    Ga secret manager (#3533) (#48)
    * GA secret manager
    
    * Fixing some issues with ga version
    
    * GA datasource as well
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secret_config_basic/main.tf
@@ -0,0 +1,18 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret-${local.name_suffix}"
+  
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
