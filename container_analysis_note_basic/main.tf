020986f384ffbe454b42f540f40af072977cc78b
The Magician <magic-modules@google.com>
2020-05-27 11:10:13 -0700

    Support containeranalysis Note and Occurence for attestation (#3564) (#52)
    * add fields to note
    
    * add occurrence
    
    * doc comments
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- container_analysis_note_basic/main.tf
+++ container_analysis_note_basic/main.tf
@@ -1,5 +1,5 @@
 resource "google_container_analysis_note" "note" {
-  name = "test-attestor-note-${local.name_suffix}"
+  name = "attestor-note-${local.name_suffix}"
   attestation_authority {
     hint {
       human_readable_name = "Attestor Note"
