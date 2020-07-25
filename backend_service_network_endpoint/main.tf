f319719df117df947232d129d22ed56a0b5dee3a
Modular Magician <magic-modules@google.com>
2020-07-25 00:55:20 +0000

    Backend service support for internet NEG backend (#3782)
    * Add ability to set global network endpoint group as backend for backend service. Make health_checks optional
    
    * PR fixes
    
    * Add encoder to remove max_utilization when neg backend
    
    * Check for global NEG in group to remove max_utilization
    
    * Add another nil check
    
    * Spacing
    
    * Docs fix
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ backend_service_network_endpoint/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_global_network_endpoint_group" "external_proxy" {
+  name                  = "network-endpoint-${local.name_suffix}"
+  network_endpoint_type = "INTERNET_FQDN_PORT"
+  default_port          = "443"
+}
+
+resource "google_compute_global_network_endpoint" "proxy" {
+  global_network_endpoint_group = google_compute_global_network_endpoint_group.external_proxy.id
+  fqdn                          = "test.example.com"
+  port                          = google_compute_global_network_endpoint_group.external_proxy.default_port
+}
+
+resource "google_compute_backend_service" "default" {
+  name                            = "backend-service-${local.name_suffix}"
+  enable_cdn                      = true
+  timeout_sec                     = 10
+  connection_draining_timeout_sec = 10
+ 
+  custom_request_headers          = ["host: ${google_compute_global_network_endpoint.proxy.fqdn}"]
+
+  backend {
+    group = google_compute_global_network_endpoint_group.external_proxy.id
+  }
+}
