1cf567272875a9a070650599630c4871a75066e4
Modular Magician <magic-modules@google.com>
2020-08-17 16:11:20 +0000

    Add support for gRPC healthchecks (#3825)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_health_check_grpc/main.tf
@@ -0,0 +1,10 @@
+resource "google_compute_region_health_check" "grpc-health-check" {
+  name = "grpc-region-health-check-${local.name_suffix}"
+
+  timeout_sec        = 1
+  check_interval_sec = 1
+
+  grpc_health_check {
+    port = "443"
+  }
+}
