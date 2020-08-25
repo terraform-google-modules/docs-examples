c0fee4a236d792158251ab017f95aa76859597e1
Modular Magician <magic-modules@google.com>
2020-08-25 20:14:45 +0000

    fix active directory domain sweeper (#3902)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- active_directory_domain_basic/main.tf
+++ active_directory_domain_basic/main.tf
@@ -1,5 +1,5 @@
 resource "google_active_directory_domain" "ad-domain" {
-  domain_name       = "name-${local.name_suffix}.org.com"
+  domain_name       = "tfgen-${local.name_suffix}.org.com"
   locations         = ["us-central1"]
   reserved_ip_range = "192.168.255.0/24" 
 }
