37cc34627a158dcd37d98e8fd9e2f2ea8fee19ea
Modular Magician <magic-modules@google.com>
2020-08-27 16:16:57 +0000

    Add Erase Windows VSS support to compute disk (#3898)
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- disk_basic/main.tf
+++ disk_basic/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_disk" "default" {
   name  = "test-disk-${local.name_suffix}"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  image = "debian-8-jessie-v20170523"
+  image = "debian-9-stretch-v20200805"
   labels = {
     environment = "dev"
   }
