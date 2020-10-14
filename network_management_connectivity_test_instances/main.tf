7c6e3ea18b791076535e544afb8bdf0f62d9ab12
Modular Magician <magic-modules@google.com>
2020-10-14 23:03:02 +0000

    use e2 vms, part 2 (#4105)
    * use e2 vms, part 2
    
    * fix tests
    
    * Update data_source_google_compute_instance_test.go
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- network_management_connectivity_test_instances/main.tf
+++ network_management_connectivity_test_instances/main.tf
@@ -13,7 +13,7 @@ resource "google_network_management_connectivity_test" "instance-test" {
 
 resource "google_compute_instance" "source" {
   name = "source-vm-${local.name_suffix}"
-  machine_type = "n1-standard-1"
+  machine_type = "e2-medium"
 
   boot_disk {
     initialize_params {
@@ -30,7 +30,7 @@ resource "google_compute_instance" "source" {
 
 resource "google_compute_instance" "destination" {
   name = "dest-vm-${local.name_suffix}"
-  machine_type = "n1-standard-1"
+  machine_type = "e2-medium"
 
   boot_disk {
     initialize_params {
