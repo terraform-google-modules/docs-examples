7d503b59032784c7c683866d8bff58519616f9be
Modular Magician <magic-modules@google.com>
2020-10-23 17:53:59 +0000

    region NEG moved to TPG (#4139)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_network_endpoint_group_cloudrun/main.tf
@@ -0,0 +1,27 @@
+// Cloud Run Example
+resource "google_compute_region_network_endpoint_group" "cloudrun_neg" {
+  name                  = "cloudrun-neg-${local.name_suffix}"
+  network_endpoint_type = "SERVERLESS"
+  region                = "us-central1"
+  cloud_run {
+    service = google_cloud_run_service.cloudrun_neg.name
+  }
+}
+
+resource "google_cloud_run_service" "cloudrun_neg" {
+  name     = "cloudrun-neg-${local.name_suffix}"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
