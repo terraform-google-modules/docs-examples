42de4835b525b42a29fe1335426542c35cd28f1c
The Magician <magic-modules@google.com>
2020-12-23 12:24:25 -0800

    Add ga version of bigquery reservation (#4342) (#116)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_reservation_basic/main.tf
@@ -0,0 +1,8 @@
+resource "google_bigquery_reservation" "reservation" {
+	name           = "reservation-${local.name_suffix}"
+	location       = "asia-northeast1"
+	// Set to 0 for testing purposes
+	// In reality this would be larger than zero
+	slot_capacity  = 0
+	ignore_idle_slots = false
+}
