bef94b928e9680219bba62ca58abc1140b71f298
The Magician <magic-modules@google.com>
2020-12-30 14:12:45 -0800

    GA packet mirroring, add filter direction, ga mirroring collector flag (#4350) (#117)
    * GA packet mirroring, add filter direction, ga mirroring collector flag
    
    * Exclude ansible
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ compute_packet_mirroring_full/main.tf
@@ -0,0 +1,76 @@
+resource "google_compute_instance" "mirror" {
+  name = "my-instance-${local.name_suffix}"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.default.id
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_packet_mirroring" "foobar" {
+  name = "my-mirroring-${local.name_suffix}"
+  description = "bar"
+  network {
+    url = google_compute_network.default.id
+  }
+  collector_ilb {
+    url = google_compute_forwarding_rule.default.id
+  }
+  mirrored_resources {
+    tags = ["foo"]
+    instances {
+      url = google_compute_instance.mirror.id
+    }
+  }
+  filter {
+    ip_protocols = ["tcp"]
+    cidr_ranges = ["0.0.0.0/0"]
+    direction = "BOTH"
+  }
+}
+resource "google_compute_network" "default" {
+  name = "my-network-${local.name_suffix}"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name = "my-subnetwork-${local.name_suffix}"
+  network       = google_compute_network.default.id
+  ip_cidr_range = "10.2.0.0/16"
+
+}
+
+resource "google_compute_region_backend_service" "default" {
+  name = "my-service-${local.name_suffix}"
+  health_checks = [google_compute_health_check.default.id]
+}
+
+resource "google_compute_health_check" "default" {
+  name = "my-healthcheck-${local.name_suffix}"
+  check_interval_sec = 1
+  timeout_sec        = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+resource "google_compute_forwarding_rule" "default" {
+  depends_on = [google_compute_subnetwork.default]
+  name       = "my-ilb-${local.name_suffix}"
+
+  is_mirroring_collector = true
+  ip_protocol            = "TCP"
+  load_balancing_scheme  = "INTERNAL"
+  backend_service        = google_compute_region_backend_service.default.id
+  all_ports              = true
+  network                = google_compute_network.default.id
+  subnetwork             = google_compute_subnetwork.default.id
+  network_tier           = "PREMIUM"
+}
