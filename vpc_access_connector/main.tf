49c6bbd5841d8f5e0edc2c590c6c3efa3f12ff66
The Magician <magic-modules@google.com>
2021-01-08 12:13:38 -0800

    Sweep vpc connector (#4367) (#119)
    * Sweep vpc connector
    
    * Update products/vpcaccess/terraform.yaml
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>

--- vpc_access_connector/main.tf
+++ vpc_access_connector/main.tf
@@ -1,5 +1,5 @@
 resource "google_vpc_access_connector" "connector" {
-  name          = "vpcconn-${local.name_suffix}"
+  name          = "vpc-con-${local.name_suffix}"
   region        = "us-central1"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
