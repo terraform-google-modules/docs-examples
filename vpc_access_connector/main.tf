06dfa078fac35cdbdc70d5de5e75f1e09b484439
Modular Magician <magic-modules@google.com>
2021-01-12 23:44:16 +0000

    fix MM locations (#4394)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- vpc_access_connector/main.tf
+++ vpc_access_connector/main.tf
@@ -1,6 +1,5 @@
 resource "google_vpc_access_connector" "connector" {
   name          = "vpc-con-${local.name_suffix}"
-  region        = "us-central1"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
 }
