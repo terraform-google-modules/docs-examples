90deffc4a88682266fc0e33586b76502af76c906
Modular Magician <magic-modules@google.com>
2021-03-25 17:33:56 +0000

    Adresssing Terraform provider issue 8732 (#4614)
    https://github.com/hashicorp/terraform-provider-google/issues/8732
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- region_target_http_proxy_basic/main.tf
+++ region_target_http_proxy_basic/main.tf
@@ -26,10 +26,11 @@ resource "google_compute_region_url_map" "default" {
 }
 
 resource "google_compute_region_backend_service" "default" {
-  region      = "us-central1"
-  name        = "backend-service-${local.name_suffix}"
-  protocol    = "HTTP"
-  timeout_sec = 10
+  region                = "us-central1"
+  name                  = "backend-service-${local.name_suffix}"
+  protocol              = "HTTP"
+  timeout_sec           = 10
+  load_balancing_scheme = "INTERNAL_MANAGED"
 
   health_checks = [google_compute_region_health_check.default.id]
 }
