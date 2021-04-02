9df8c28ba5f7252dc47447ef93215ef48dc63295
Modular Magician <magic-modules@google.com>
2021-04-02 15:50:43 +0000

    Revert "Updated datatype for mtu" (#4643)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ interconnect_attachment_basic/main.tf
@@ -0,0 +1,20 @@
+resource "google_compute_interconnect_attachment" "on_prem" {
+  name                     = "on-prem-attachment-${local.name_suffix}"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+  type                     = "PARTNER"
+  router                   = google_compute_router.foobar.id
+  mtu                      = 1500
+}
+
+resource "google_compute_router" "foobar" {
+  name    = "router-${local.name_suffix}"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn = 16550
+  }
+}
+
+resource "google_compute_network" "foobar" {
+  name                    = "network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
