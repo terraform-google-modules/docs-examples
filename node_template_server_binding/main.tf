2e4b5c14f9a50fb8ef2cb4c935d06e3a21b3f9f3
Modular Magician <magic-modules@google.com>
2021-04-16 16:03:49 +0000

    Removed beta provider from node template test (#4704)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- node_template_server_binding/main.tf
+++ node_template_server_binding/main.tf
@@ -1,16 +1,8 @@
-provider "google-beta" {
-  region = "us-central1"
-  zone   = "us-central1-a"
-}
-
 data "google_compute_node_types" "central1a" {
-  provider = google-beta
   zone     = "us-central1-a"
 }
 
 resource "google_compute_node_template" "template" {
-  provider = google-beta
-
   name      = "soletenant-with-licenses-${local.name_suffix}"
   region    = "us-central1"
   node_type = "n1-node-96-624"
