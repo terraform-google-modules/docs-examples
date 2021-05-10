fb20d2233c833e88473dba87df06495a9aa95469
The Magician <magic-modules@google.com>
2021-05-10 14:08:30 -0700

    Feature/add pubsub schema resource (#4741) (#138)
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>

---
+++ pubsub_schema_basic/main.tf
@@ -0,0 +1,5 @@
+resource "google_pubsub_schema" "example" {
+  name = "example-${local.name_suffix}"
+  type = "AVRO"
+  definition = "{\n  \"type\" : \"record\",\n  \"name\" : \"Avro\",\n  \"fields\" : [\n    {\n      \"name\" : \"StringField\",\n      \"type\" : \"string\"\n    },\n    {\n      \"name\" : \"IntField\",\n      \"type\" : \"int\"\n    }\n  ]\n}\n"
+}
