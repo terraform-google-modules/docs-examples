f1be506646aab1462724f1def3c154bf29c60290
Modular Magician <magic-modules@google.com>
2021-07-09 17:16:54 +0000

    port media edge resources over from EAP provider (#4958)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_services_edge_cache_origin_advanced/main.tf
@@ -0,0 +1,35 @@
+
+resource "google_network_services_edge_cache_origin" "fallback" {
+  name                 = "fallback-${local.name_suffix}"
+  origin_address       = "gs://media-edge-fallback"
+  description          = "The default bucket for media edge test"
+  max_attempts         = 3
+  protocol = "HTTP"
+  port = 80
+
+  retry_conditions = [
+    "CONNECT_FAILURE",
+    "NOT_FOUND",
+    "HTTP_5XX"
+  ]
+  timeout {
+    connect_timeout = "10s"
+    max_attempts_timeout = "10s"
+    response_timeout = "10s"
+  }
+}
+
+resource "google_network_services_edge_cache_origin" "default" {
+  name                 = "default-${local.name_suffix}"
+  origin_address       = "gs://media-edge-default"
+  failover_origin      = google_network_services_edge_cache_origin.fallback.id
+  description          = "The default bucket for media edge test"
+  max_attempts         = 2
+  labels = {
+    a = "b"
+  }
+
+  timeout {
+    connect_timeout = "10s"
+  }
+}
---
+++ network_services_edge_cache_origin_basic/main.tf
@@ -0,0 +1,5 @@
+resource "google_network_services_edge_cache_origin" "default" {
+  name                 = "default-${local.name_suffix}"
+  origin_address       = "gs://media-edge-default"
+  description          = "The default bucket for media edge test"
+}
