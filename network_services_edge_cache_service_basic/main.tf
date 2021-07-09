6cd786552dbdb1269f2d8ef8d3df287f40dc2772
The Magician <magic-modules@google.com>
2021-07-09 12:17:07 -0500

    port media edge resources over from EAP provider (#4958) (#152)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_services_edge_cache_service_basic/main.tf
@@ -0,0 +1,50 @@
+resource "google_storage_bucket" "dest" {
+  name = "my-bucket-${local.name_suffix}"
+
+  force_destroy = true
+}
+
+resource "google_network_services_edge_cache_origin" "instance" {
+  name                 = "my-origin-${local.name_suffix}"
+  origin_address       = google_storage_bucket.dest.url
+  description          = "The default bucket for media edge test"
+  max_attempts         = 2
+  timeout {
+    connect_timeout = "10s"
+  }
+}
+
+resource "google_network_services_edge_cache_service" "instance" {
+  name                 = "my-service-${local.name_suffix}"
+  description          = "some description"
+  routing {
+    host_rule {
+      description = "host rule description"
+      hosts = ["sslcert.tf-test.club"]
+      path_matcher = "routes"
+    }
+    path_matcher {
+      name = "routes"
+      route_rule {
+        description = "a route rule to match against"
+        priority = 1
+        match_rule {
+          prefix_match = "/"
+        }
+        origin = google_network_services_edge_cache_origin.instance.name
+        route_action {
+          cdn_policy {
+              cache_mode = "CACHE_ALL_STATIC"
+              default_ttl = "3600s"
+          }
+        }
+        header_action {
+          response_header_to_add {
+            header_name = "x-cache-status"
+            header_value = "{cdn_cache_status}"
+          }
+        }
+      }
+    }
+  }
+}
