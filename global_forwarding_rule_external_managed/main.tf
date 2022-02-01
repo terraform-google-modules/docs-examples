cb513127cf2b5df2ae990d9010001054820af4e7
Modular Magician <magic-modules@google.com>
2022-02-01 19:20:37 +0000

    Enable EXTERNAL_MANAGED resource  examples for GA (#5660)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ global_forwarding_rule_external_managed/main.tf
@@ -0,0 +1,41 @@
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "global-rule-${local.name_suffix}"
+  target                = google_compute_target_http_proxy.default.id
+  port_range            = "80"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name        = "target-proxy-${local.name_suffix}"
+  description = "a description"
+  url_map     = google_compute_url_map.default.id
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "url-map-target-proxy-${local.name_suffix}"
+  description     = "a description"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name                  = "backend-${local.name_suffix}"
+  port_name             = "http"
+  protocol              = "HTTP"
+  timeout_sec           = 10
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+}
