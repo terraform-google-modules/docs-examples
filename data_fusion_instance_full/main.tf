5630ec130c9f58c0e36923caeccacc5cb8a7a608
The Magician <magic-modules@google.com>
2022-02-10 16:57:46 -0800

    Promote `google_datafusion_instance` to GA (#5683) (#191)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: upodroid <cy@borg.dev>

--- data_fusion_instance_full/main.tf
+++ data_fusion_instance_full/main.tf
@@ -1,5 +1,4 @@
 resource "google_data_fusion_instance" "extended_instance" {
-  provider = "google-beta"
   name = "my-instance-${local.name_suffix}"
   description = "My Data Fusion instance"
   region = "us-central1"
@@ -14,4 +13,9 @@ resource "google_data_fusion_instance" "extended_instance" {
     network = "default"
     ip_allocation = "10.89.48.0/22"
   }
+  version = "6.3.0"
+  dataproc_service_account = data.google_app_engine_default_service_account.default.email
+}
+
+data "google_app_engine_default_service_account" "default" {
 }
