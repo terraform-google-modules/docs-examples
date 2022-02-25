ef7569b5334f1609a1a3f6dc9302cfe85fc1f136
The Magician <magic-modules@google.com>
2022-02-25 12:22:16 -0800

    Added region tags for inclusion on cgc cloud SQL docs (#5749) (#194)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- sql_database_basic/main.tf
+++ sql_database_basic/main.tf
@@ -3,6 +3,7 @@ resource "google_sql_database" "database" {
   instance = google_sql_database_instance.instance.name
 }
 
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
 resource "google_sql_database_instance" "instance" {
   name             = "my-database-instance-${local.name_suffix}"
   region           = "us-central1"
