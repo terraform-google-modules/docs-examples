fe4293398b6fc6c0a5302be38a847832121f2477
Modular Magician <magic-modules@google.com>
2022-03-04 17:28:20 +0000

    Add support for manual builds, git source for webhook/pubsub triggered builds and filter field (#5738)
    * add filter support for cloudbuild
    
    * Add support for sourceToBuild and gitFileSource. Add examples for pubsub_config, webhook_config trigger and manual mode
    
    * Add support for sourceToBuild and gitFileSource. Add examples for pubsub_config, webhook_config trigger and manual mode
    
    * Fix description for field uri in sourceToBuild to be required
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudbuild_trigger_manual/main.tf
@@ -0,0 +1,17 @@
+
+resource "google_cloudbuild_trigger" "manual-trigger" {
+  name        = "manual-build"
+
+  source_to_build {
+    uri       = "https://hashicorp/terraform-provider-google-beta"
+    ref       = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       = "https://hashicorp/terraform-provider-google-beta"
+    revision  = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+}
