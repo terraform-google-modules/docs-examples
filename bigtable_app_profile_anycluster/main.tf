920fb2a111a711c51e8d929f3b55ea4ebdb831bc
The Magician <magic-modules@google.com>
2022-04-01 15:04:08 -0500

    Update examples for Cloud Bigtable AppProfile. (#5871) (#205)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- bigtable_app_profile_anycluster/main.tf
+++ bigtable_app_profile_anycluster/main.tf
@@ -2,10 +2,22 @@ resource "google_bigtable_instance" "instance" {
   name = "bt-instance-${local.name_suffix}"
   cluster {
     cluster_id   = "cluster-1"
+    zone         = "us-central1-a"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }
+  cluster {
+    cluster_id   = "cluster-2"
     zone         = "us-central1-b"
     num_nodes    = 3
     storage_type = "HDD"
   }
+  cluster {
+    cluster_id   = "cluster-3"
+    zone         = "us-central1-c"
+    num_nodes    = 3
+    storage_type = "HDD"
+  }  
 
   deletion_protection  = "false"
 }
@@ -14,11 +26,8 @@ resource "google_bigtable_app_profile" "ap" {
   instance       = google_bigtable_instance.instance.name
   app_profile_id = "bt-profile-${local.name_suffix}"
 
-  // Requests will be routed to the following cluster.
-  single_cluster_routing {
-    cluster_id                 = "cluster-1"
-    allow_transactional_writes = true
-  }
+  // Requests will be routed to any of the 3 clusters.
+  multi_cluster_routing_use_any = true
 
-  ignore_warnings = true
+  ignore_warnings               = true
 }
