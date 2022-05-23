6820e71dd34c00a3eef2d50ca4fb545cbfe31c38
Modular Magician <magic-modules@google.com>
2022-05-23 20:45:47 +0000

    Tf dns samples (#6024)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dns_managed_zone_quickstart/main.tf
@@ -0,0 +1,54 @@
+# to setup a web-server
+resource "google_compute_instance" "default" {
+  name         = "dns-compute-instance-${local.name_suffix}"
+  machine_type = "g1-small"
+  zone         = "us-central1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  metadata_startup_script = <<-EOF
+  sudo apt-get update && \
+  sudo apt-get install apache2 -y && \
+  echo "<!doctype html><html><body><h1>Hello World!</h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+# to allow http traffic
+resource "google_compute_firewall" "default" {
+  name    = "allow-http-traffic-${local.name_suffix}"
+  network = "default"
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# to create a DNS zone
+resource "google_dns_managed_zone" "default" {
+  name          = "example-zone-googlecloudexample-${local.name_suffix}"
+  dns_name      = "googlecloudexample.com."
+  description   = "Example DNS zone"
+  force_destroy = "true"
+}
+
+# to register web-server's ip address in DNS
+resource "google_dns_record_set" "default" {
+  name         = google_dns_managed_zone.default.dns_name
+  managed_zone = google_dns_managed_zone.default.name
+  type         = "A"
+  ttl          = 300
+  rrdatas = [
+    google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  ]
+}
