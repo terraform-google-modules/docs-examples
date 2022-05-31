dc7e30ae5db0b9f6aca1564a59c2736cdaba8e2a
The Magician <magic-modules@google.com>
2022-05-31 14:54:02 -0700

    Add support for cacheKeyPolicy in BackendBuckets (#6043) (#223)
    * Add support for cacheKeyPolicy in BackendBuckets
    
    * Add an example of include_http_headers
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ backend_bucket_query_string_whitelist/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_backend_bucket" "image_backend" {
+  name        = "image-backend-bucket-${local.name_suffix}"
+  description = "Contains beautiful images"
+  bucket_name = google_storage_bucket.image_bucket.name
+  enable_cdn  = true
+  cdn_policy {
+    cache_key_policy {
+        query_string_whitelist = ["image-version"]
+    }
+  }
+}
+
+resource "google_storage_bucket" "image_bucket" {
+  name     = "image-backend-bucket-${local.name_suffix}"
+  location = "EU"
+}
