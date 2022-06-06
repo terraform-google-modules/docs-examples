0ba5b8878f3a9a66029f8544397bff8637ab4372
Modular Magician <magic-modules@google.com>
2022-06-06 16:05:45 +0000

    Support for user_labels in google_monitoring_slo (#6070)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- monitoring_service_custom/main.tf
+++ monitoring_service_custom/main.tf
@@ -5,4 +5,9 @@ resource "google_monitoring_custom_service" "custom" {
   telemetry {
   	resource_name = "//product.googleapis.com/foo/foo/services/test-${local.name_suffix}"
   }
+
+  user_labels = {
+    my_key       = "my_value"
+    my_other_key = "my_other_value"
+  }
 }
