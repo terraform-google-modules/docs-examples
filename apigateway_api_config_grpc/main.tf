4628f30513341254031f4f0f854b08b10c5ecb85
The Magician <magic-modules@google.com>
2022-06-14 11:23:24 -0700

    Add support for API Gateway gRPC (#6120) (#228)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ apigateway_api_config_grpc/main.tf
@@ -0,0 +1,42 @@
+resource "google_api_gateway_api" "api_cfg" {
+  provider = google-beta
+  api_id = "api-cfg-${local.name_suffix}"
+}
+
+resource "google_api_gateway_api_config" "api_cfg" {
+  provider = google-beta
+  api = google_api_gateway_api.api_cfg.api_id
+  api_config_id = "cfg-${local.name_suffix}"
+
+  grpc_services {
+    file_descriptor_set {
+      path = "api_descriptor.pb"
+      contents = filebase64("test-fixtures/apigateway/api_descriptor.pb")
+    }
+  }
+  managed_service_configs {
+    path = "api_config.yaml"
+    contents = base64encode(<<-EOF
+      type: google.api.Service
+      config_version: 3
+      name: ${google_api_gateway_api.api_cfg.managed_service}
+      title: gRPC API example
+      apis:
+        - name: endpoints.examples.bookstore.Bookstore
+      usage:
+        rules:
+        - selector: endpoints.examples.bookstore.Bookstore.ListShelves
+          allow_unregistered_calls: true
+      backend:
+        rules:
+          - selector: "*"
+            address: grpcs://example.com
+            disable_auth: true
+
+    EOF
+    )
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
