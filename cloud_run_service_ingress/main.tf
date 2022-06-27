cde2b2b57f4811cf040dbb8c7b16399524149b3b
Modular Magician <magic-modules@google.com>
2022-06-27 20:03:56 +0000

    example for controlling cloudrun service ingress option (#6168)
    * Add tf example for securing Ingress options on Cloud Run
    
    * update region tags
    
    * review comments from @megan07
    
    * update region tags as requested by @pattishin
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloud_run_service_ingress/main.tf
@@ -0,0 +1,23 @@
+resource "google_cloud_run_service" "default" {
+    name     = "ingress-service-${local.name_suffix}"
+    location = "us-central1"
+
+    template {
+      spec {
+        containers {
+          image = "gcr.io/cloudrun/hello" #public image for your service
+        }
+      }
+    }
+    traffic {
+      percent         = 100
+      latest_revision = true
+    }
+    metadata {
+      annotations = {
+        # For valid annotation values and descriptions, see
+        # https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
+        "run.googleapis.com/ingress" = "internal"
+      }
+    }
+}
