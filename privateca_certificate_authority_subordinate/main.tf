abbec80d8adca2edeff6ce52ebec9eeb12435ed6
The Magician <magic-modules@google.com>
2022-06-29 09:32:12 -0700

    Support subordinate CA activation via first and third party issuers. (#6161) (#235)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- privateca_certificate_authority_subordinate/main.tf
+++ privateca_certificate_authority_subordinate/main.tf
@@ -1,10 +1,51 @@
+resource "google_privateca_certificate_authority" "root-ca" {
+  pool = "ca-pool-${local.name_suffix}"
+  certificate_authority_id = "my-certificate-authority-${local.name_suffix}-root"
+  location = "us-central1"
+  deletion_protection = false
+  ignore_active_certificates_on_deletion = true
+  config {
+    subject_config {
+      subject {
+        organization = "HashiCorp"
+        common_name = "my-certificate-authority"
+      }
+      subject_alt_name {
+        dns_names = ["hashicorp.com"]
+      }
+    }
+    x509_config {
+      ca_options {
+        # is_ca *MUST* be true for certificate authorities
+        is_ca = true
+      }
+      key_usage {
+        base_key_usage {
+          # cert_sign and crl_sign *MUST* be true for certificate authorities
+          cert_sign = true
+          crl_sign = true
+        }
+        extended_key_usage {
+          server_auth = false
+        }
+      }
+    }
+  }
+  key_spec {
+    algorithm = "RSA_PKCS1_4096_SHA256"
+  }
+}
+
 resource "google_privateca_certificate_authority" "default" {
   // This example assumes this pool already exists.
   // Pools cannot be deleted in normal test circumstances, so we depend on static pools
   pool = "ca-pool-${local.name_suffix}"
-  certificate_authority_id = "my-certificate-authority-${local.name_suffix}"
+  certificate_authority_id = "my-certificate-authority-${local.name_suffix}-sub"
   location = "us-central1-${local.name_suffix}"
   deletion_protection = "true-${local.name_suffix}"
+  subordinate_config {
+    certificate_authority = google_privateca_certificate_authority.root-ca.name
+  }
   config {
     subject_config {
       subject {
