4746cf04d12e6d3804a4b0f79b6bf67a7e7c960d
Modular Magician <magic-modules@google.com>
2022-07-14 20:17:00 +0000

    Add Metastore Federation Resource (#6215)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dataproc_metastore_federation_basic/main.tf
@@ -0,0 +1,25 @@
+resource "google_dataproc_metastore_federation" "default" {
+  provider      = google-beta
+  location      = "us-central1"
+  federation_id = "fed-${local.name_suffix}"
+  version       = "3.1.2"
+
+  backend_metastores {
+      rank           = "1"
+      name           = google_dataproc_metastore_service.default.id
+      metastore_type = "DATAPROC_METASTORE" 
+    }
+}
+
+resource "google_dataproc_metastore_service" "default" {
+  provider   = google-beta
+  service_id = "fed-${local.name_suffix}"
+  location   = "us-central1"
+  tier       = "DEVELOPER"
+
+
+  hive_metastore_config {
+    version           = "3.1.2"
+    endpoint_protocol = "GRPC"
+  }
+}
