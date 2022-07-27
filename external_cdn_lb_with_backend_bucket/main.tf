e53de67d61ddf3400075e1fe4abbf6dc7aab9cd8
The Magician <magic-modules@google.com>
2022-07-27 10:23:35 -0700

    Making example fit the doc (#6324) (#263)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- external_cdn_lb_with_backend_bucket/main.tf
+++ external_cdn_lb_with_backend_bucket/main.tf
@@ -1,29 +1,70 @@
 # CDN load balancer with Cloud bucket as backend
 
-
-
-# VPC
-resource "google_compute_network" "default" {
-  name                    = "cdn-network-${local.name_suffix}"
-  auto_create_subnetworks = false
+# Cloud Storage bucket
+resource "google_storage_bucket" "default" {
+  name                        = "my-bucket-${local.name_suffix}"
+  location                    = "us-east1"
+  uniform_bucket_level_access = true
+  storage_class               = "STANDARD"
+  // delete bucket and contents on destroy.
+  force_destroy = true
+  // Assign specialty files
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
 
-# backend subnet
-resource "google_compute_subnetwork" "default" {
-  name          = "cdn-subnet-${local.name_suffix}"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
+
+# make bucket public
+resource "google_storage_bucket_iam_member" "default" {
+  bucket = google_storage_bucket.default.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+resource "google_storage_bucket_object" "index_page" {
+  name    = "index-page-${local.name_suffix}"
+  bucket  = google_storage_bucket.default.name
+  content = <<-EOT
+    <html><body>
+    <h1>Congratulations on setting up Google Cloud CDN with Storage backend!</h1>
+    </body></html>
+  EOT
+}
+
+resource "google_storage_bucket_object" "error_page" {
+  name    = "404-page-${local.name_suffix}"
+  bucket  = google_storage_bucket.default.name
+  content = <<-EOT
+    <html><body>
+    <h1>404 Error: Object you are looking for is no longer available!</h1>
+    </body></html>
+  EOT
+}
+
+# image object for testing, try to access http://<your_lb_ip_address>/test.jpg
+resource "google_storage_bucket_object" "test_image" {
+  name = "test-object-${local.name_suffix}"
+  # Uncomment and add valid path to an object.
+  #  source       = "/path/to/an/object"
+  #  content_type = "image/jpeg"
+
+  # Delete after uncommenting above source and content_type attributes
+  content      = "Data as string to be uploaded"
+  content_type = "text/plain"
+
+  bucket = google_storage_bucket.default.name
 }
 
 # reserve IP address
 resource "google_compute_global_address" "default" {
-  name     = "cdn-static-ip-${local.name_suffix}"
+  name = "example-ip-${local.name_suffix}"
 }
 
 # forwarding rule
 resource "google_compute_global_forwarding_rule" "default" {
-  name                  = "cdn-forwarding-rule-${local.name_suffix}"
+  name                  = "http-lb-forwarding-rule-${local.name_suffix}"
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
@@ -33,19 +74,19 @@ resource "google_compute_global_forwarding_rule" "default" {
 
 # http proxy
 resource "google_compute_target_http_proxy" "default" {
-  name     = "cdn-target-http-proxy-${local.name_suffix}"
-  url_map  = google_compute_url_map.default.id
+  name    = "http-lb-proxy-${local.name_suffix}"
+  url_map = google_compute_url_map.default.id
 }
 
 # url map
 resource "google_compute_url_map" "default" {
-  name            = "cdn-url-map-${local.name_suffix}"
+  name            = "http-lb-${local.name_suffix}"
   default_service = google_compute_backend_bucket.default.id
 }
 
 # backend bucket with CDN policy with default ttl settings
 resource "google_compute_backend_bucket" "default" {
-  name        = "image-backend-bucket-${local.name_suffix}"
+  name        = "cat-backend-bucket-${local.name_suffix}"
   description = "Contains beautiful images"
   bucket_name = google_storage_bucket.default.name
   enable_cdn  = true
@@ -57,59 +98,4 @@ resource "google_compute_backend_bucket" "default" {
     negative_caching  = true
     serve_while_stale = 86400
   }
-}
-
-# Cloud Storage bucket
-resource "google_storage_bucket" "default" {
-  name                        = "cdn-backend-storage-bucket-${local.name_suffix}"
-  location                    = "US"
-  uniform_bucket_level_access = true
-  // delete bucket and contents on destroy.
-  force_destroy = true
-  // Assign specialty files
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-# make bucket public
-resource "google_storage_bucket_iam_member" "default" {
-  bucket = google_storage_bucket.default.name
-  role   = "roles/storage.objectViewer"
-  member = "allUsers"
-}
-
-resource "google_storage_bucket_object" "index_page" {
-  name       = "index-page-${local.name_suffix}"
-  bucket     = google_storage_bucket.default.name
-  content = <<-EOT
-    <html><body>
-    <h1>Congratulations on setting up Google Cloud CDN with Storage backend!</h1>
-    </body></html>
-  EOT
-}
-
-resource "google_storage_bucket_object" "error_page" {
-  name       = "404-page-${local.name_suffix}"
-  bucket     = google_storage_bucket.default.name
-  content  = <<-EOT
-    <html><body>
-    <h1>404 Error: Object you are looking for is no longer available!</h1>
-    </body></html>
-  EOT
-}
-
-# image object for testing, try to access http://<your_lb_ip_address>/test.jpg
-resource "google_storage_bucket_object" "test_image" {
-  name         = "test-object-${local.name_suffix}"
-# Uncomment and add valid path to an object.
-#  source       = "/path/to/an/object"
-#  content_type = "image/jpeg"
-
-# Delete after uncommenting above source and content_type attributes
-  content      = "Data as string to be uploaded"
-  content_type = "text/plain"
-
-  bucket       = google_storage_bucket.default.name
 }
