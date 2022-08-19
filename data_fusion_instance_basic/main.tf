f599d9ebf1ad6ce76523c6f895ce6851904bdee0
Modular Magician <magic-modules@google.com>
2022-08-19 18:03:38 +0000

    Add prober_test_run to prevent provisioning service netowrking for datafusion tests (#6426)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- data_fusion_instance_basic/main.tf
+++ data_fusion_instance_basic/main.tf
@@ -2,4 +2,8 @@ resource "google_data_fusion_instance" "basic_instance" {
   name = "my-instance-${local.name_suffix}"
   region = "us-central1"
   type = "BASIC"
+  # Mark for testing to avoid service networking connection usage that is not cleaned up
+  options = {
+    prober_test_run = "true"
+  }
 }
