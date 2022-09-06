09b3e29e70ecb20a5d7f14c70be94d6be1e836d0
Modular Magician <magic-modules@google.com>
2022-09-06 16:13:01 +0000

    new service - analytics hub - data exchange resource (#6421)
    Co-authored-by: Shuya Ma <87669292+shuyama1@users.noreply.github.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_analyticshub_data_exchange_basic/main.tf
@@ -0,0 +1,7 @@
+resource "google_bigquery_analytics_hub_data_exchange" "data_exchange" {
+  provider = google-beta
+  location         = "US"
+  data_exchange_id = "my_data_exchange-${local.name_suffix}"
+  display_name     = "my_data_exchange-${local.name_suffix}"
+  description      = "example data exchange-${local.name_suffix}"
+}
