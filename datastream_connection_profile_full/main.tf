3e94f495af6c2e9f06ca2df16cd4624cf8f5554c
The Magician <magic-modules@google.com>
2022-09-06 09:39:54 -0700

    Added support for Datastream service & for ConnectionProfile resource (#6479) (#281)
    * Added datastream connection profile
    
    * Updated resource and tests to get to passing basic/full tests
    
    * Added update test
    
    * Corrected license dates
    
    * Moved url params into parameters array
    
    * Removed long timeouts
    
    * Ensured that update test actually tests update
    
    * Made update test move from gcs bucket to postgresql database instance
    
    * Switched to handwritten operation due to unexpected error structure
    
    * Added authorized networks to sql database instance
    
    * fixed whitespace
    
    * Re-added async definitions
    
    Async definitions are used to figure out whether to call async code even if the async code isn't autogenerated
    
    * Added postgresql password to importstateverifyignore
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ datastream_connection_profile_full/main.tf
@@ -0,0 +1,17 @@
+resource "google_datastream_connection_profile" "default" {
+	display_name          = "Connection profile"
+	location              = "us-central1"
+	connection_profile_id = "my-profile-${local.name_suffix}"
+
+	gcs_profile {
+		bucket    = "my-bucket"
+		root_path = "/path"
+	}
+
+	forward_ssh_connectivity {
+		hostname = "google.com"
+		username = "my-user"
+		port     = 8022
+		password = "swordfish"
+	}
+}
