c909508802115415718e3d0657549db3eaf3d060
Modular Magician <magic-modules@google.com>
2022-09-07 16:05:15 +0000

    Add filestore_snapshot resource (#6498)
    * Add filestore_snapshot resource
    
    * Fix API link for filestore_snapshot resource
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ filestore_snapshot_basic/main.tf
@@ -0,0 +1,21 @@
+resource "google_filestore_snapshot" "snapshot" {
+  name     = "test-snapshot-${local.name_suffix}"
+  instance = google_filestore_instance.instance.name
+  location = "us-central1"
+}
+
+resource "google_filestore_instance" "instance" {
+  name     = "test-instance-for-snapshot-${local.name_suffix}"
+  location = "us-central1"
+  tier     = "ENTERPRISE"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "share1"
+  }
+
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV4"]
+  }
+}
