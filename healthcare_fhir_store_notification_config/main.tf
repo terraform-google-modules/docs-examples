52d9c5c1f0691a714e6a8802f2773f0ff666a6c1
Modular Magician <magic-modules@google.com>
2022-09-26 16:09:04 +0000

    Support healthcare fhir new notifications config (#6471)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ healthcare_fhir_store_notification_config/main.tf
@@ -0,0 +1,28 @@
+resource "google_healthcare_fhir_store" "default" {
+  name    = "example-fhir-store-${local.name_suffix}"
+  dataset = google_healthcare_dataset.dataset.id
+  version = "R4"
+
+  enable_update_create          = false
+  disable_referential_integrity = false
+  disable_resource_versioning   = false
+  enable_history_import         = false
+
+  labels = {
+    label1 = "labelvalue1"
+  }
+
+  notification_configs {
+    pubsub_topic = "${google_pubsub_topic.topic.id}" 
+    send_full_resource = true
+  }
+}
+
+resource "google_pubsub_topic" "topic" {
+  name     = "fhir-notifications-${local.name_suffix}"
+}
+
+resource "google_healthcare_dataset" "dataset" {
+  name     = "example-dataset-${local.name_suffix}"
+  location = "us-central1"
+}
