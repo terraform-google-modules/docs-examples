07c91f48df691c86fc8f039cee88fe06a50c26ab
The Magician <magic-modules@google.com>
2022-10-03 14:03:10 -0700

    Add the vertex endpoint resource. (#6583) (#295)
    * Add the vertex endpoint resource.
    
    * Add a KMS key to the test.
    
    * Add an iam member for encypter decrypter role on kms key.
    
    * Use a bootstrapped test network.
    
    * Make bootstrapped network a data source.
    
    * Use long form for self link.
    
    * Add property overrides.
    
    * Make accelerator_type a string.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ vertex_ai_endpoint_network/main.tf
@@ -0,0 +1,41 @@
+resource "google_vertex_ai_endpoint" "endpoint" {
+  display_name = "sample-endpoint"
+  description  = "A sample vertex endpoint"
+  location     = "us-central1"
+  labels       = {
+    label-one = "value-one"
+  }
+  network      = "projects/${data.google_project.project.number}/global/networks/${data.google_compute_network.vertex_network.name}"
+  encryption_spec {
+    kms_key_name = "kms-name-${local.name_suffix}"
+  }
+  depends_on   = [
+    google_service_networking_connection.vertex_vpc_connection
+  ]
+}
+
+resource "google_service_networking_connection" "vertex_vpc_connection" {
+  network                 = data.google_compute_network.vertex_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.vertex_range.name]
+}
+
+resource "google_compute_global_address" "vertex_range" {
+  name          = "address-name-${local.name_suffix}"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = data.google_compute_network.vertex_network.id
+}
+
+data "google_compute_network" "vertex_network" {
+  name       = "network-name-${local.name_suffix}"
+}
+
+resource "google_kms_crypto_key_iam_member" "crypto_key" {
+  crypto_key_id = "kms-name-${local.name_suffix}"
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
+}
+
+data "google_project" "project" {}
