01422af92c507e007fef573f12a9a6c2f735a1e4
The Magician <magic-modules@google.com>
2022-10-04 09:27:12 -0700

    Added support for regional target TCP proxy and removed some unused fields from target regional HTTP/HTTPS proxy. (#6622) (#296)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_target_tcp_proxy_basic/main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_region_target_tcp_proxy" "default" {
+  provider        = google-beta
+  name            = "test-proxy-${local.name_suffix}"
+  region          = "europe-west4"
+  backend_service = google_compute_region_backend_service.default.id
+}
+
+resource "google_compute_region_backend_service" "default" {
+  provider    = google-beta
+  name        = "backend-service-${local.name_suffix}"
+  protocol    = "TCP"
+  timeout_sec = 10
+  region      = "europe-west4"
+
+  health_checks         = [google_compute_region_health_check.default.id]
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider           = google-beta
+  name               = "health-check-${local.name_suffix}"
+  region             = "europe-west4"
+  timeout_sec        = 1
+  check_interval_sec = 1
+  
+  tcp_health_check {
+    port = "80"
+  }
+}
