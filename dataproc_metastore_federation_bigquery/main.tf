014fbcdfa5e01fa2d92afe5044b8a9273c4acea0
The Magician <magic-modules@google.com>
2022-10-05 10:47:56 -0700

    dataproc metastore - add BQ support (#6631) (#297)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- dataproc_metastore_federation_bigquery/main.tf
+++ dataproc_metastore_federation_bigquery/main.tf
@@ -1,8 +1,14 @@
 resource "google_dataproc_metastore_federation" "default" {
   provider      = google-beta
   location      = "us-central1"
-  federation_id = "fed-1-${local.name_suffix}"
+  federation_id = "fed-2-${local.name_suffix}"
   version       = "3.1.2"
+
+  backend_metastores {
+    rank           = "2"
+    name           = data.google_project.project.id
+    metastore_type = "BIGQUERY" 
+  }
 
   backend_metastores {
     rank           = "1"
@@ -13,7 +19,7 @@ resource "google_dataproc_metastore_federation" "default" {
 
 resource "google_dataproc_metastore_service" "default" {
   provider   = google-beta
-  service_id = "fed-1-${local.name_suffix}"
+  service_id = "fed-2-${local.name_suffix}"
   location   = "us-central1"
   tier       = "DEVELOPER"
 
@@ -22,4 +28,8 @@ resource "google_dataproc_metastore_service" "default" {
     version           = "3.1.2"
     endpoint_protocol = "GRPC"
   }
+}
+
+data "google_project" "project" {
+  provider      = google-beta
 }
