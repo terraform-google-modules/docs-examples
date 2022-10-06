f511eeef6739a5ffd3e0f2ee0ebdd2f0cd0fa033
Modular Magician <magic-modules@google.com>
2022-10-06 23:33:53 +0000

    Added CMEK support for cloud data fusion (#6659)
    Co-authored-by: Niranjan KL <niranjankl@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ data_fusion_instance_cmek/main.tf
@@ -0,0 +1,32 @@
+resource "google_data_fusion_instance" "basic_cmek" {
+  name   = "my-instance-${local.name_suffix}"
+  region = "us-central1"
+  type   = "BASIC"
+
+  crypto_key_config {
+    key_reference = google_kms_crypto_key.crypto_key.id
+  }
+
+  depends_on = [google_kms_crypto_key_iam_binding.crypto_key_binding]
+}
+
+resource "google_kms_crypto_key" "crypto_key" {
+  name     = "my-instance-${local.name_suffix}"
+  key_ring = google_kms_key_ring.key_ring.id
+}
+
+resource "google_kms_key_ring" "key_ring" {
+  name     = "my-instance-${local.name_suffix}"
+  location = "us-central1"
+}
+
+resource "google_kms_crypto_key_iam_binding" "crypto_key_binding" {
+  crypto_key_id = google_kms_crypto_key.crypto_key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+
+  members = [
+    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-datafusion.iam.gserviceaccount.com"
+  ]
+}
+
+data "google_project" "project" {}
