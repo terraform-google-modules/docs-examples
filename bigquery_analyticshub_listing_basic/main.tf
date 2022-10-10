d7ad2f04c85943c478b9ca7a1df136c157563a6d
The Magician <magic-modules@google.com>
2022-10-10 15:10:00 -0700

    bq analytics hub - add listing resource (#6564) (#301)
    * bq analytics hub - add listing resource
    
    * iam import
    
    * change enum to string
    
    * CR comments
    
    * rebase + CR comments
    
    * only if beta
    
    * make template
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_analyticshub_listing_basic/main.tf
@@ -0,0 +1,28 @@
+resource "google_bigquery_analytics_hub_data_exchange" "listing" {
+  provider = google-beta
+  location         = "US"
+  data_exchange_id = "my_data_exchange-${local.name_suffix}"
+  display_name     = "my_data_exchange-${local.name_suffix}"
+  description      = "example data exchange-${local.name_suffix}"
+}
+
+resource "google_bigquery_analytics_hub_listing" "listing" {
+  provider = google-beta
+  location         = "US"
+  data_exchange_id = google_bigquery_analytics_hub_data_exchange.listing.data_exchange_id
+  listing_id       = "my_listing-${local.name_suffix}"
+  display_name     = "my_listing-${local.name_suffix}"
+  description      = "example data exchange-${local.name_suffix}"
+
+  bigquery_dataset {
+    dataset = google_bigquery_dataset.listing.id
+  }
+}
+
+resource "google_bigquery_dataset" "listing" {
+  provider = google-beta
+  dataset_id                  = "my_listing-${local.name_suffix}"
+  friendly_name               = "my_listing-${local.name_suffix}"
+  description                 = "example data exchange-${local.name_suffix}"
+  location                    = "US"
+}
