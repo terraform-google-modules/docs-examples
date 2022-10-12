47b353fb07a8a17e91915bcbd86bd2779469aa01
The Magician <magic-modules@google.com>
2022-10-12 11:05:07 -0700

    Alloydb cluster (#6507) (#302)
    * basic resource + import
    
    * add computed sources
    
    * more args
    
    * fix network
    
    * fix types for duration
    
    * instance
    
    * Update mmv1/products/alloydb/api.yaml
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Update mmv1/products/alloydb/api.yaml
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * Update mmv1/products/alloydb/api.yaml
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * create network for test
    
    * remove excludes + machineConfig
    
    * cluster_id
    
    * cluster_id
    
    * revert
    
    * use parent resource
    
    * use parent resource
    
    * add selflink
    
    * alloydb tests
    
    * beta
    
    * import
    
    * imports
    
    * add conflicts
    
    * revert
    
    * revert
    
    * revert
    
    * revert
    
    * Apply suggestions from code review
    
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    
    * reduce only to cluster resource
    
    * Apply suggestions from code review
    
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>
    
    * remove fields
    
    * full example
    
    * full test + validations
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * remove unneeded arg
    
    * CR - remove unspecified enums
    
    * remove unneeded arg
    
    * remove unneeded arg
    
    * tests passing
    
    * Update mmv1/products/alloydb/api.yaml
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    * remove fields
    
    * remove kms
    
    * remove kms
    
    * fix rebase
    
    * CR comments
    
    * CR comments
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Co-authored-by: Scott Suarez <ScottSuarez@google.com>

---
+++ alloydb_cluster_full/main.tf
@@ -0,0 +1,49 @@
+resource "google_alloydb_cluster" "full" {
+  provider     = google-beta
+  cluster_id   = "alloydb-cluster-full-${local.name_suffix}"
+  location     = "us-central1"
+  network      = "projects/${data.google_project.project.number}/global/networks/${google_compute_network.default.name}"
+
+  initial_user {
+    user     = "alloydb-cluster-full-${local.name_suffix}"
+    password = "alloydb-cluster-full-${local.name_suffix}"
+  }
+
+  automated_backup_policy {
+    location      = "us-central1"
+    backup_window = "1800s"
+    enabled       = true
+
+    weekly_schedule {
+      days_of_week = ["MONDAY"]
+
+      start_times {
+        hours   = 23
+        minutes = 0
+        seconds = 0
+        nanos   = 0
+      }
+    }
+
+    quantity_based_retention {
+      count = 1
+    }
+
+    labels = {
+      test = "alloydb-cluster-full-${local.name_suffix}"
+    }
+  }
+
+  labels = {
+    test = "alloydb-cluster-full-${local.name_suffix}"
+  }
+}
+
+data "google_project" "project" {
+  provider = google-beta
+}
+
+resource "google_compute_network" "default" {
+  provider = google-beta
+  name = "alloydb-cluster-full-${local.name_suffix}"
+}
