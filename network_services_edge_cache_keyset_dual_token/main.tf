e282da7f24080f23a321c3bbe7a39fb6bc912874
Modular Magician <magic-modules@google.com>
2022-11-15 17:26:46 +0000

    `network_services`: Dual-Token Authentication Support (#6751)
    Resolves https://github.com/hashicorp/terraform-provider-google/issues/12775
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_services_edge_cache_keyset_dual_token/main.tf
@@ -0,0 +1,25 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret-name-${local.name_suffix}"
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  secret = google_secret_manager_secret.secret-basic.id
+
+  secret_data = "secret-data"
+}
+
+resource "google_network_services_edge_cache_keyset" "default" {
+  name        = "default-${local.name_suffix}"
+  description = "The default keyset"
+  public_key {
+    id      = "my-public-key"
+    managed = true
+  }
+  validation_shared_keys {
+    secret_version = google_secret_manager_secret_version.secret-version-basic.id
+  }
+}
