3a26611bc1a1ba584d34b68cfc329dc2e9697bef
The Magician <magic-modules@google.com>
2022-11-29 10:55:28 -0800

    datafusion - add args + iam support (#6270) (#325)
    Closes https://github.com/hashicorp/terraform-provider-google/issues/10536
    Closes https://github.com/hashicorp/terraform-provider-google/issues/9065
    Closes https://github.com/hashicorp/terraform-provider-google/issues/9068
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- data_fusion_instance_basic/main.tf
+++ data_fusion_instance_basic/main.tf
@@ -1,7 +1,7 @@
 resource "google_data_fusion_instance" "basic_instance" {
-  name = "my-instance-${local.name_suffix}"
+  name   = "my-instance-${local.name_suffix}"
   region = "us-central1"
-  type = "BASIC"
+  type   = "BASIC"
   # Mark for testing to avoid service networking connection usage that is not cleaned up
   options = {
     prober_test_run = "true"
