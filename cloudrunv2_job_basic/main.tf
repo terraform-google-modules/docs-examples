4459c24bf181efcf55da9c23c11d50fd77c83ddb
The Magician <magic-modules@google.com>
2022-11-30 17:02:16 -0800

    Cloudrun v2 job (#6852) (#329)
    resolve https://github.com/hashicorp/terraform-provider-google/issues/11743
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_job_basic/main.tf
@@ -0,0 +1,13 @@
+resource "google_cloud_run_v2_job" "default" {
+  name     = "cloudrun-job-${local.name_suffix}"
+  location = "us-central1"
+  launch_stage = "BETA"
+
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
