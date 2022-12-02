7bc40d4ae985bf90293d77cf797652b1fd24aae6
Modular Magician <magic-modules@google.com>
2022-12-02 20:35:43 +0000

    Cloudrun v2 service (#6850)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_service_sql/main.tf
@@ -0,0 +1,79 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service-${local.name_suffix}"
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+  
+  template {
+    scaling {
+      max_instance_count = 2
+    }
+  
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.instance.connection_name]
+      }
+    }
+
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+      env {
+        name = "FOO"
+        value = "bar"
+      }
+      env {
+        name = "SECRET_ENV_VAR"
+        value_source {
+          secret_key_ref {
+            secret = google_secret_manager_secret.secret.secret_id
+            version = "1"
+          }
+        }
+      }
+      volume_mounts {
+        name = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+    }
+  }
+
+  traffic {
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+  depends_on = [google_secret_manager_secret_version.secret-version-data]
+}
+
+data "google_project" "project" {
+}
+
+resource "google_secret_manager_secret" "secret" {
+  secret_id = "secret-1-${local.name_suffix}"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-data" {
+  secret = google_secret_manager_secret.secret.name
+  secret_data = "secret-data"
+}
+
+resource "google_secret_manager_secret_iam_member" "secret-access" {
+  secret_id = google_secret_manager_secret.secret.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  depends_on = [google_secret_manager_secret.secret]
+}
+
+resource "google_sql_database_instance" "instance" {
+  name             = "cloudrun-sql-${local.name_suffix}"
+  region           = "us-central1"
+  database_version = "MYSQL_5_7"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "false"
+}
