803141aa31531fef2cf2fcbd2db170902b1e0b3d
The Magician <magic-modules@google.com>
2023-01-18 16:10:16 -0800

    sql: added several fields to `google_sql_source_representation_instance` resource (#7143) (#346)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- sql_source_representation_instance_basic/main.tf
+++ sql_source_representation_instance_basic/main.tf
@@ -1,7 +1,10 @@
 resource "google_sql_source_representation_instance" "instance" {
-  name             = "my-instance-${local.name_suffix}"
-  region           = "us-central1"
-  database_version = "MYSQL_8_0"
-  host             = "10.20.30.40"
-  port             = 3306
+  name               = "my-instance-${local.name_suffix}"
+  region             = "us-central1"
+  database_version   = "MYSQL_8_0"
+  host               = "10.20.30.40"
+  port               = 3306
+  username           = "some-user"
+  password           = "password-for-the-user"
+  dump_file_path     = "gs://replica-bucket/source-database.sql.gz"
 }
