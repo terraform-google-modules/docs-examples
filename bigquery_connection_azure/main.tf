f5249444a888bd32e33064aeece25a6d875494a2
The Magician <magic-modules@google.com>
2023-01-31 10:31:53 -0800

    Add Azure odic support for BQ Omni connections (#7188) (#351)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Cameron Thornton <camthornton@google.com>

--- bigquery_connection_azure/main.tf
+++ bigquery_connection_azure/main.tf
@@ -5,5 +5,6 @@ resource "google_bigquery_connection" "connection" {
    description   = "a riveting description"
    azure {
       customer_tenant_id = "customer-tenant-id-${local.name_suffix}"
+      federated_application_client_id = "b43eeeee-eeee-eeee-eeee-a480155501ce-${local.name_suffix}"
    }
 }
