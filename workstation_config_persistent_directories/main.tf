f2559076755c0e1a61cc57d770e0994a1b147b21
The Magician <magic-modules@google.com>
2023-02-24 08:10:51 -0800

    Cloud Workstations - Workstation Config (#7017) (#364)
    * Add WorkstationConfig resource
    
    * indent correctly
    
    * add the workstationConfig
    
    * Add WorkstationConfig
    
    * Create workstation_config_basic.tf.erb
    
    * Create resource_workstations_workstation_config_test.go.erb
    
    * fix indent
    
    * remove duplicate displayName field
    
    * add `workstationClusterId` param
    
    * set host attribute block
    
    * set the `workstation_cluster_id`
    
    * fix create
    
    * use a default for the container property
    
    * add input true
    
    * set api defaults
    
    * set `host.gceInstance.bootDiskSizeGb` to be computed
    
    * fix machine_type attribute
    
    * fix typo's
    
    * lower case funcs
    
    * Update mmv1/third_party/terraform/tests/resource_workstations_workstation_config_test.go.erb
    
    
    
    * add location
    
    * add skip test to examples
    
    * add tests to run sequentially instead of paralell
    
    * set `<% unless version == 'ga' -%>`
    
    * Remove extra tests
    
    * align google_workstations_workstation_config resource in update test
    
    * Revert "align google_workstations_workstation_config resource in update test"
    
    This reverts commit 37a4af300e0d78b4110c8029f09511ae1957e6c9.
    
    * Revert "Remove extra tests"
    
    This reverts commit 1fc074464b3fea0293884bbda86725a81e2f08e6.
    
    * revert skip test
    
    * align tests and remove update label
    
    * add encryptionKey attribute
    
    * indent the description
    
    * add encryption key test
    
    * Use a static `account_id` for KMS SA
    
    * address comments on pr
    
    * set some extra defaults
    
    * add 2 new tests to test host and persistent_directories
    
    * Move back up
    
    * have a single set of unless/end
    
    * remove data google project
    
    * Remove faulty import
    
    * remove duplicate output and fmt
    
    * remove output field on displayName
    
    * add displayName update test
    
    * Fix update of display name
    
    ---------
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Riley Karson <rileykarson@google.com>

---
+++ workstation_config_persistent_directories/main.tf
@@ -0,0 +1,56 @@
+resource "google_compute_network" "default" {
+  provider                = google-beta
+  name                    = "workstation-cluster-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  provider      = google-beta
+  name          = "workstation-cluster-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.default.name
+}
+
+resource "google_workstations_workstation_cluster" "default" {
+  provider               = google-beta
+  workstation_cluster_id = "workstation-cluster-${local.name_suffix}"
+  network                = google_compute_network.default.id
+  subnetwork             = google_compute_subnetwork.default.id
+  location               = "us-central1"
+  
+  labels = {
+    "label" = "key"
+  }
+
+  annotations = {
+    label-one = "value-one"
+  }
+}
+
+resource "google_workstations_workstation_config" "default" {
+  provider               = google-beta
+  workstation_config_id  = "workstation-config-${local.name_suffix}"
+  workstation_cluster_id = google_workstations_workstation_cluster.default.workstation_cluster_id
+  location   		         = "us-central1"
+
+  host {
+    gce_instance {
+      machine_type                = "e2-standard-4"
+      boot_disk_size_gb           = 35
+      disable_public_ip_addresses = true
+      shielded_instance_config {
+        enable_secure_boot = true
+        enable_vtpm        = true
+      }
+    }
+  }
+
+  persistent_directories {
+    mount_path = "/home"
+    gce_pd {
+      size_gb        = 200
+      reclaim_policy = "DELETE"
+    }
+  }
+}
