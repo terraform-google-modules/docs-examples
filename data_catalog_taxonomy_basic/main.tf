f0741d4a74affa89e58f3369511ed4c4b1c91c26
Modular Magician <magic-modules@google.com>
2023-02-27 19:35:22 +0000

    datacatalog - bump Taxonomy and PolicyTag to ga (#6989)
    * update Taxonomy and PolicyTag to ga
    
    * update Taxonomy and PolicyTag to ga
    
    * refactor
    
    * refactor
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- data_catalog_taxonomy_basic/main.tf
+++ data_catalog_taxonomy_basic/main.tf
@@ -1,7 +1,5 @@
 resource "google_data_catalog_taxonomy" "basic_taxonomy" {
-  provider = google-beta
-  region = "us"
-  display_name =  "my_display_name-${local.name_suffix}"
+  display_name =  "my_taxonomy-${local.name_suffix}"
   description = "A collection of policy tags"
   activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
 }
