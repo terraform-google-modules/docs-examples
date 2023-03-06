9c97b6e25b0b000fdf3d462281517e44593761ec
Modular Magician <magic-modules@google.com>
2023-03-06 19:02:47 +0000

    Artifact Registry: add virtual repository and remote repository support (#7333)
    * Artifact Registry: Virtual + Remote Repositories
    
    * Artifact Registry: Examples + beta v1 endpoint
    
    * Artifact Registry: Removed unused fields from examples
    
    * Artifact Registry: Generate missing exactly_one_of fields
    
    * Artifact Registry: Input-only RR field + Handwritten update tests
    
    * Artifact Registry: Input-only propagation
    
    * Artifact Registry: proper merge
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ artifact_registry_repository_remote/main.tf
@@ -0,0 +1,14 @@
+resource "google_artifact_registry_repository" "my-repo" {
+  provider      = google-beta
+  location      = "us-central1"
+  repository_id = "my-repository-${local.name_suffix}"
+  description   = "example remote docker repository-${local.name_suffix}"
+  format        = "DOCKER"
+  mode          = "REMOTE_REPOSITORY"
+  remote_repository_config {
+    description = "docker hub"
+    docker_repository {
+      public_repository = "DOCKER_HUB"
+    }
+  }
+}
---
+++ artifact_registry_repository_virtual/main.tf
@@ -0,0 +1,24 @@
+resource "google_artifact_registry_repository" "my-repo-upstream" {
+  provider      = google-beta
+  location      = "us-central1"
+  repository_id = "my-repository-upstream-${local.name_suffix}"
+  description   = "example docker repository (upstream source)-${local.name_suffix}"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  depends_on    = []
+  provider      = google-beta
+  location      = "us-central1"
+  repository_id = "my-repository-${local.name_suffix}"
+  description   = "example virtual docker repository-${local.name_suffix}"
+  format        = "DOCKER"
+  mode          = "VIRTUAL_REPOSITORY"
+  virtual_repository_config {
+    upstream_policies {
+      id          = "my-repository-upstream-${local.name_suffix}"
+      repository  = google_artifact_registry_repository.my-repo-upstream.id
+      priority    = 1
+    }
+  }
+}
