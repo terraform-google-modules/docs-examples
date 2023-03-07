c3dfe809e8a59090dd34629aba25c8c5a5c629ce
Modular Magician <magic-modules@google.com>
2023-03-07 19:59:47 +0000

    Bootstrap network cleanup (#7367)
    * Update bootstrapped networks for alloydb
    
    * Update bootstrapped networks for redis
    
    * Update bootstrapped networks for vertexai
    
    * Update comment for BootstrapSharedTestNetwork to be more clear
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ alloydb_backup_basic/main.tf
@@ -0,0 +1,39 @@
+resource "google_alloydb_backup" "default" {
+  location     = "us-central1"
+  backup_id    = "alloydb-backup-${local.name_suffix}"
+  cluster_name = google_alloydb_cluster.default.name
+
+  depends_on = [google_alloydb_instance.default]
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "alloydb-cluster-${local.name_suffix}"
+  location   = "us-central1"
+  network    = data.google_compute_network.default.id
+}
+
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "alloydb-instance-${local.name_suffix}"
+  instance_type = "PRIMARY"
+
+  depends_on = [google_service_networking_connection.vpc_connection]
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "alloydb-cluster-${local.name_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = data.google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = data.google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
+
+data "google_compute_network" "default" {
+  name = "alloydb-network-${local.name_suffix}"
+}
