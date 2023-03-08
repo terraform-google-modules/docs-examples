2cd757d7455b3e63ab0545753e9e2549ddb3d30a
The Magician <magic-modules@google.com>
2023-03-08 13:59:55 -0800

    Add support for accelerators to google_datafusion_instance (#6851) (#377)
    Added support to the field for Data Fusion, updated corresponding generated and handwritten tests, and added a custom diffsuppressfunc.
    
    Currently suggests a manual fix to a potential diff issue, will be able to update down line based on feedback from API team.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- data_fusion_instance_full/main.tf
+++ data_fusion_instance_full/main.tf
@@ -18,6 +18,10 @@ resource "google_data_fusion_instance" "extended_instance" {
     ip_allocation = "${google_compute_global_address.private_ip_alloc.address}/${google_compute_global_address.private_ip_alloc.prefix_length}"
   }
 
+  accelerators {
+    accelerator_type = "CDC"
+    state = "ENABLED"
+  }
   -${local.name_suffix}
 }
 
