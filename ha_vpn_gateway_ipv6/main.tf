a54618c4e6dc0765f3694ad15a69b30008068ae4
Modular Magician <magic-modules@google.com>
2023-03-29 17:49:19 +0000

    Support IPV6 for HA VPN. (#7525)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ ha_vpn_gateway_ipv6/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_ha_vpn_gateway" "ha_gateway1" {
+  region   = "us-central1"
+  name     = "ha-vpn-1-${local.name_suffix}"
+  network  = google_compute_network.network1.id
+  stack_type = "IPV4_IPV6"
+}
+
+resource "google_compute_network" "network1" {
+  name                    = "network1-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
