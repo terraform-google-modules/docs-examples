4712ad5da88eb9d4247633688ef9a262853e94af
Modular Magician <magic-modules@google.com>
2023-04-04 20:11:58 +0000

    artifactregistry: add docker tag immutability config (#7609)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ artifact_registry_repository_docker/main.tf
@@ -0,0 +1,10 @@
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "my-repository-${local.name_suffix}"
+  description   = "example docker repository-${local.name_suffix}"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+}
