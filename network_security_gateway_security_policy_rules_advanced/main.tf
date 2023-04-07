d43173d99431aa53d4da00c756b9c742955ae067
Modular Magician <magic-modules@google.com>
2023-04-07 21:38:58 +0000

    Make network security gateway names singular (#7666)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_security_gateway_security_policy_rules_advanced/main.tf
@@ -0,0 +1,20 @@
+resource "google_network_security_gateway_security_policy" "default" {
+  provider    = google-beta
+  name        = "my-gateway-security-policy-${local.name_suffix}"
+  location    = "us-central1"
+  description = "gateway security policy created to be used as reference by the rule."
+}
+
+resource "google_network_security_gateway_security_policy_rule" "default" {
+  provider                = google-beta
+  name                    = "my-gateway-security-policy-rule-${local.name_suffix}"
+  location                = "us-central1"
+  gateway_security_policy = google_network_security_gateway_security_policy.default.name
+  enabled                 = true  
+  description             = "my description"
+  priority                = 0
+  session_matcher         = "host() == 'example.com'"
+  application_matcher     = "request.method == 'POST'"
+  tls_inspection_enabled  = false
+  basic_profile           = "ALLOW"
+}
