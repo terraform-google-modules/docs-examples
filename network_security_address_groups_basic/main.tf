c9e6dfd9e4ebf2c054fa7c6e225814a3d157a984
Modular Magician <magic-modules@google.com>
2023-04-24 14:42:55 +0000

    Create the resource Address groups (#7713)
    * adding the resource, the examples and the test
    
    * fix identation
    
    * updating the examples
    
    * adding in the tests the labels to include this field into covered and removing self-link
    
    * fixing required fields and adding one more item into the update test
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_security_address_groups_basic/main.tf
@@ -0,0 +1,8 @@
+resource "google_network_security_address_group" "default" {
+  provider    = google-beta
+  name        = "my-address-groups-${local.name_suffix}"
+  location    = "us-central1"
+  type        = "IPV4"
+  capacity    = "100"
+  items       = ["208.80.154.224/32"]
+}
