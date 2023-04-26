4974937481c6a5eb146995d7d03e4fcb9e946549
The Magician <magic-modules@google.com>
2023-04-26 12:58:38 -0700

    Update certificate manager certificate to deal with locations (#7825) (#418)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- certificate_manager_self_managed_certificate_regional/main.tf
+++ certificate_manager_self_managed_certificate_regional/main.tf
@@ -1,7 +1,7 @@
 resource "google_certificate_manager_certificate" "default" {
   name        = "self-managed-cert-${local.name_suffix}"
-  description = "Global cert"
-  scope       = "EDGE_CACHE"
+  description = "Regional cert"
+  location    = "us-central1"
   self_managed {
     pem_certificate = file("test-fixtures/certificatemanager/cert.pem")
     pem_private_key = file("test-fixtures/certificatemanager/private-key.pem")
