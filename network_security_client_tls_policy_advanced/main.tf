f8e6c589daee2a7f5ba28b36753e33728c7aea34
The Magician <magic-modules@google.com>
2023-04-27 17:58:27 -0700

    Client tls policy (#7807) (#421)
    * Configuration for traffic director Mesh resource.
    
    * Add more tests for Mesh resource
    
    * Use new provider for test
    
    * Configuration for service binding.
    
    * Add hand written test for service binding.
    
    * Revert "Add hand written test for service binding."
    
    This reverts commit 367449af370e7e09509cba20d24ed2c152251072.
    
    * Update service binding yaml.
    
    * reduce timeouts.
    
    * Update mmv1/products/networkservices/ServiceBinding.yaml
    
    
    
    * Service binding update test.
    
    * Fix compile error.
    
    * Mark the Service Binding resource immutable.
    
    * Add configuration for client tls policy.
    
    * Remove extra files.
    
    * Add more tests.
    
    * Address PR comments.
    
    * Revert "Remove extra files."
    
    This reverts commit bf65cf3e37fc94f0d1bad1bfb00768b846420522.
    
    * Address comments.
    
    * Address comments.
    
    ---------
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Madhura Phadnis <madhurap@google.com>
    Co-authored-by: Sam Levenick <slevenick@google.com>

---
+++ network_security_client_tls_policy_advanced/main.tf
@@ -0,0 +1,23 @@
+resource "google_network_security_client_tls_policy" "default" {
+  provider               = google-beta
+  name                   = "my-client-tls-policy-${local.name_suffix}"
+  labels                 = {
+    foo = "bar"
+  }
+  description            = "my description"
+  client_certificate {
+    certificate_provider_instance {
+        plugin_instance = "google_cloud_private_spiffe"
+      }
+    }
+  server_validation_ca {
+    grpc_endpoint {
+      target_uri = "unix:mypath"
+    }
+  }
+  server_validation_ca {
+    grpc_endpoint {
+      target_uri = "unix:mypath1"
+    }
+  }
+}
