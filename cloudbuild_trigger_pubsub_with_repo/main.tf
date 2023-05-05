4012e9c604562e9b589bd8a551cf205c85ba598c
Modular Magician <magic-modules@google.com>
2023-05-05 16:35:01 +0000

    Adds repository field to git_file_source and source_to_build of googl… (#7790)
    Co-authored-by: Mario Machado <mariomachado@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudbuild_trigger_pubsub_with_repo/main.tf
@@ -0,0 +1,45 @@
+resource "google_cloudbuildv2_connection" "my-connection" {
+  provider = google-beta
+  location = "us-central1"
+  name = "my-connection"
+
+  github_config {
+    app_installation_id = 123123-${local.name_suffix}
+    authorizer_credential {
+      oauth_token_secret_version = "projects/my-project/secrets/github-pat-secret/versions/latest-${local.name_suffix}"
+    }
+  }
+}
+
+resource "google_cloudbuildv2_repository" "my-repository" {
+  provider = google-beta
+  name = "my-repo"
+  parent_connection = google_cloudbuildv2_connection.my-connection.id
+  remote_uri = "https://github.com/myuser/my-repo.git-${local.name_suffix}"
+}
+
+resource "google_pubsub_topic" "mytopic" {
+  provider = google-beta
+  name = "mytopic"
+}
+
+resource "google_cloudbuild_trigger" "pubsub-with-repo-trigger" {
+  provider = google-beta
+  name = "pubsub-with-repo-trigger"
+  location = "us-central1"
+
+  pubsub_config {
+    topic = google_pubsub_topic.mytopic.id
+  }
+  source_to_build {
+    repository = google_cloudbuildv2_repository.my-repository.id
+    ref = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+  git_file_source {
+    path = "cloudbuild.yaml"
+    repository = google_cloudbuildv2_repository.my-repository.id
+    revision = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+}
