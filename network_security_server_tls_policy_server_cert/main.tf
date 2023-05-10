fbe47695535f04e8d9c3cb4429a4894f329c7479
Modular Magician <magic-modules@google.com>
2023-05-10 18:38:04 +0000

    Server tls policy (#7822)
    * Configuration for traffic director Mesh resource.
    
    * Add more tests for Mesh resource
    
    * Use new provider for test
    
    * Configuration for service binding.
    
    * Add hand written test for service binding.
    
    * Revert "Add hand written test for service binding."
    
    This reverts commit 367449af370e7e09509cba20d24ed2c152251072.
    
    * Update service binding yaml.
    
    * reduce timeouts.
    
    * Update mmv1/products/networkservices/ServiceBinding.yaml
    
    Co-authored-by: Sam Levenick <slevenick@google.com>
    
    * Service binding update test.
    
    * Fix compile error.
    
    * Mark the Service Binding resource immutable.
    
    * Add configuration for server tls policy.
    
    * Remove extra files.
    
    * Add tests.
    
    * Revert "Remove extra files."
    
    This reverts commit 0791d448f775b7c9c705ad4f6ff35d45b05b114c.
    
    * Update mmv1/third_party/terraform/tests/resource_network_security_server_tls_policy_test.go.erb
    
    Co-authored-by: Sarah French <15078782+SarahFrench@users.noreply.github.com>
    
    * Address PR comments.
    
    * Server Tls Policy
    
    * Address PR comments.
    
    * Address PR comments.
    
    ---------
    
    Co-authored-by: Madhura Phadnis <madhurap@google.com>
    Co-authored-by: Sam Levenick <slevenick@google.com>
    Co-authored-by: Sarah French <15078782+SarahFrench@users.noreply.github.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_security_server_tls_policy_server_cert/main.tf
@@ -0,0 +1,15 @@
+resource "google_network_security_server_tls_policy" "default" {
+  provider               = google-beta
+  name                   = "my-server-tls-policy-${local.name_suffix}"
+  labels                 = {
+    foo = "bar"
+  }
+  description            = "my description"
+  location               = "global"
+  allow_open             = "false"
+  server_certificate {
+    grpc_endpoint {
+        target_uri = "unix:mypath"
+      }
+  }
+}
