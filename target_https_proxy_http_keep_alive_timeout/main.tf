cf69ab8e39e817a3f3d3a959aa7998048d155891
The Magician <magic-modules@google.com>
2023-06-27 09:22:57 -0700

    Add field httpKeepAliveTimeoutSec to resources google_compute_target_http_proxy and google_compute_target_https_proxy (beta) (#8131) (#453)
    * HTTP Keep-alive Timeout: Add http_keep_alive_timeout_sec to Target HTTP Proxy and Target HTTPS Proxy
    
    * HTTP Keep-alive Timeout: Add target_http_proxy_http_keep_alive_timeout and target_https_proxy_http_keep_alive_timeout examples
    
    * HTTP Keep-alive Timeout: Fix indent for cloudloadbalancing_target_http_proxy_http_keep_alive_timeout
    
    * HTTP Keep-alive Timeout: Fix examples generation
    
    * HTTP Keep-alive Timeout: Specofy google-beta provider for HTTP keep-alive timeout tests
    
    * HTTP Keep-alive Timeout: Update example target http proxy name
    
    * HTTP Keep-alive Timeout: Mark httpKeepAliveTimeoutSec with beta
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ target_https_proxy_http_keep_alive_timeout/main.tf
@@ -0,0 +1,56 @@
+resource "google_compute_target_https_proxy" "default" {
+  provider                    = google-beta
+  name                        = "test-http-keep-alive-timeout-proxy-${local.name_suffix}"
+  http_keep_alive_timeout_sec = 120
+  url_map                     = google_compute_url_map.default.id
+  ssl_certificates            = [google_compute_ssl_certificate.default.id]
+}
+
+resource "google_compute_ssl_certificate" "default" {
+  provider    = google-beta
+  name        = "my-certificate-${local.name_suffix}"
+  private_key = file("../static/ssl_cert/test.key")
+  certificate = file("../static/ssl_cert/test.crt")
+}
+
+resource "google_compute_url_map" "default" {
+  provider   = google-beta
+  name        = "url-map-${local.name_suffix}"
+  description = "a description"
+
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  provider              = google-beta
+  name                  = "backend-service-${local.name_suffix}"
+  port_name             = "http"
+  protocol              = "HTTP"
+  timeout_sec           = 10
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  provider           = google-beta
+  name               = "http-health-check-${local.name_suffix}"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
