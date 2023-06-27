702af427c140c92d420466879bc846ead5216500
Modular Magician <magic-modules@google.com>
2023-06-27 19:40:50 +0000

    Fix Filestore quota usage (#8211)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- filestore_snapshot_basic/main.tf
+++ filestore_snapshot_basic/main.tf
@@ -1,12 +1,12 @@
 resource "google_filestore_snapshot" "snapshot" {
   name     = "test-snapshot-${local.name_suffix}"
   instance = google_filestore_instance.instance.name
-  location = "us-central1"
+  location = "us-east1"
 }
 
 resource "google_filestore_instance" "instance" {
   name     = "test-instance-for-snapshot-${local.name_suffix}"
-  location = "us-central1"
+  location = "us-east1"
   tier     = "ENTERPRISE"
 
   file_shares {
