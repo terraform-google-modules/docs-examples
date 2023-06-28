032bccad5437fb270809063968d85039069a5b6c
Modular Magician <magic-modules@google.com>
2023-06-28 18:25:30 +0000

    Put workstation test key ring in workstation location (#8223)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- workstation_config_encryption_key/main.tf
+++ workstation_config_encryption_key/main.tf
@@ -1,11 +1,13 @@
 resource "google_compute_network" "default" {
-  provider                = google-beta
+  provider = google-beta
+
   name                    = "workstation-cluster-${local.name_suffix}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "default" {
-  provider      = google-beta
+  provider = google-beta
+
   name          = "workstation-cluster-${local.name_suffix}"
   ip_cidr_range = "10.0.0.0/24"
   region        = "us-central1"
@@ -13,7 +15,8 @@ resource "google_compute_subnetwork" "default" {
 }
 
 resource "google_workstations_workstation_cluster" "default" {
-  provider               = google-beta
+  provider = google-beta
+
   workstation_cluster_id = "workstation-cluster-${local.name_suffix}"
   network                = google_compute_network.default.id
   subnetwork             = google_compute_subnetwork.default.id
@@ -29,25 +32,29 @@ resource "google_workstations_workstation_cluster" "default" {
 }
 
 resource "google_kms_key_ring" "default" {
-  name     = "workstation-cluster-${local.name_suffix}"
-  location = "global"
   provider = google-beta
+
+  name     = "workstation-cluster-${local.name_suffix}"
+  location = "us-central1"
 }
 
 resource "google_kms_crypto_key" "default" {
+  provider = google-beta
+
   name            = "workstation-cluster-${local.name_suffix}"
   key_ring        = google_kms_key_ring.default.id
-  provider        = google-beta
 }
 
 resource "google_service_account" "default" {
+  provider = google-beta
+
   account_id   = "my-account-${local.name_suffix}"
   display_name = "Service Account"
-  provider = google-beta
 }
 
 resource "google_workstations_workstation_config" "default" {
   provider               = google-beta
+
   workstation_config_id  = "workstation-config-${local.name_suffix}"
   workstation_cluster_id = google_workstations_workstation_cluster.default.workstation_cluster_id
   location   		         = "us-central1"
