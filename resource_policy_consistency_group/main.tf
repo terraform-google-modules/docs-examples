e4a9dfbb3aded728e9fdf739f2098ced3b34f36c
Modular Magician <magic-modules@google.com>
2023-07-03 10:49:12 +0000

    Promote asynchronous disk replication resources to GA. (#8232)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- resource_policy_consistency_group/main.tf
+++ resource_policy_consistency_group/main.tf
@@ -1,6 +1,4 @@
 resource "google_compute_resource_policy" "cgroup" {
-  provider = google-beta
-
   name   = "gce-policy-${local.name_suffix}"
   region = "europe-west1"
   disk_consistency_group_policy {
