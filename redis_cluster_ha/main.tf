12e849627cafce184afec2479b44a49b16a7442c
The Magician <magic-modules@google.com>
2023-08-28 12:19:36 -0400

    Redis cluster terraform support (#8567) (#510)
    * Redis cluster terraform support
    
    * updating cluster resource
    
    * updating cluster resource
    
    * updating cluster resource
    
    * updating cluster resource
    
    * updating cluster resource
    
    * updating cluster resource
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update SCPolicy
    
    * Update Service connection policy
    
    * Update Service connection policy
    
    ---------
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    Co-authored-by: Himani Khanduja <khimani@google.com>

---
+++ redis_cluster_ha/main.tf
@@ -0,0 +1,41 @@
+resource "google_redis_cluster" "cluster-ha" {
+  provider       = google-beta
+  name           = "ha-cluster-${local.name_suffix}"
+  shard_count    = 3
+  psc_configs {
+    network = google_compute_network.producer_net.id
+  }
+  region = "us-central1"
+  replica_count = 1
+  transit_encryption_mode = "TRANSIT_ENCRYPTION_MODE_DISABLED"
+  authorization_mode = "AUTH_MODE_DISABLED"
+  depends_on = [
+    google_network_connectivity_service_connection_policy.default
+  ]
+}
+
+resource "google_network_connectivity_service_connection_policy" "default" {
+  provider = google-beta
+  name = "mypolicy-${local.name_suffix}"
+  location = "us-central1"
+  service_class = "gcp-memorystore-redis"
+  description   = "my basic service connection policy"
+  network = google_compute_network.producer_net.id
+  psc_config {
+    subnetworks = [google_compute_subnetwork.producer_subnet.id]
+  }
+}
+
+resource "google_compute_subnetwork" "producer_subnet" {
+  provider      = google-beta
+  name          = "mysubnet-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.248/29"
+  region        = "us-central1"
+  network       = google_compute_network.producer_net.id
+}
+
+resource "google_compute_network" "producer_net" {
+  provider                = google-beta
+  name                    = "mynetwork-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
