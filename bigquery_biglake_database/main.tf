5b853a1ad94afe214e77d412c19c41879bb125f7
Modular Magician <magic-modules@google.com>
2023-08-28 22:49:40 +0000

    Add: BigLake API Database Resource (#8753)
    * Add: BigLake api Database Resource
    
    * add: database example
    
    * add: database update test
    
    * update: test fields
    
    * update: mark `catalog_id` required + immutable
    
    * update: `type` field. use string, mark required
    
    * update: mark hiveoptions req
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_biglake_database/main.tf
@@ -0,0 +1,30 @@
+resource "google_biglake_catalog" "catalog" {
+    name = "my_catalog-${local.name_suffix}"
+    location = "US"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "my_bucket-${local.name_suffix}"
+  location                    = "US"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "metadata_folder" {
+  name    = "metadata/"
+  content = " "
+  bucket  = google_storage_bucket.bucket.name
+}
+
+resource "google_biglake_database" "database"  {
+    name = "my_database-${local.name_suffix}"
+    catalog_id = google_biglake_catalog.catalog.name
+    location = google_biglake_catalog.catalog.location
+    type = "HIVE"
+    hive_options {
+        location_uri = "gs://${google_storage_bucket.bucket.name}/${google_storage_bucket_object.metadata_folder.name}"
+        parameters = {
+          "owner": "John Doe"
+        }
+    }
+}
