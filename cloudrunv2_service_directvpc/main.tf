ae9f9176e24d738a61872e873109a92fb7bff2eb
The Magician <magic-modules@google.com>
2023-09-15 16:00:16 -0400

    Adds Direct VPC egress support for Cloud Run V2 resources (#8932) (#533)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_service_directvpc/main.tf
@@ -0,0 +1,18 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service-${local.name_suffix}"
+  location = "us-central1"
+  launch_stage = "BETA"
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+    vpc_access{
+      network_interfaces {
+        network = "default"
+        subnetwork = "default"
+        tags = ["tag1", "tag2", "tag3"]
+      }
+      egress = "ALL_TRAFFIC"
+    }
+  }
+}
