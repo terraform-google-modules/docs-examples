6a0b7bc9ecc44a1214d91315a9d5d9798e0bbd38
The Magician <magic-modules@google.com>
2023-10-03 15:40:50 -0400

    added server tls policy example configuration for mtls (#9104) (#556)
    [upstream:74f3808bbf499a1324c61835429993fd59cea515]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_security_server_tls_policy_mtls/main.tf
@@ -0,0 +1,41 @@
+data "google_project" "project" {
+  provider = google-beta
+}
+
+resource "google_network_security_server_tls_policy" "default" {
+  provider = google-beta
+  name     = "my-server-tls-policy-${local.name_suffix}"
+
+  description = "my description"
+  location    = "global"
+  allow_open  = "false"
+
+  mtls_policy {
+    client_validation_mode         = "REJECT_INVALID"
+    client_validation_trust_config = "projects/${data.google_project.project.number}/locations/global/trustConfigs/${google_certificate_manager_trust_config.default.name}"
+  }
+
+  labels = {
+    foo = "bar"
+  }
+}
+
+resource "google_certificate_manager_trust_config" "default" {
+  provider    = google-beta
+  name        = "my-trust-config-${local.name_suffix}"
+  description = "sample trust config description"
+  location    = "global"
+
+  trust_stores {
+    trust_anchors {
+      pem_certificate = file("test-fixtures/ca_cert.pem")
+    }
+    intermediate_cas {
+      pem_certificate = file("test-fixtures/ca_cert.pem")
+    }
+  }
+
+  labels = {
+    foo = "bar"
+  }
+}
