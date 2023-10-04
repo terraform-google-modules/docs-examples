dd5e8547c6b801e46b9c3a84d4932030af0bc51c
The Magician <magic-modules@google.com>
2023-10-04 13:40:19 -0400

    Subnet allowSubnetCidrRoutesOverlap (#9102) (#558)
    [upstream:0c9bef9f18781b93585fe3edf844f0ccd0641aa2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ subnetwork_cidr_overlap/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_subnetwork" "subnetwork-cidr-overlap" {
+  provider = google-beta
+
+  name                             = "subnet-cidr-overlap-${local.name_suffix}"
+  region                           = "us-west2"
+  ip_cidr_range                    = "192.168.1.0/24"
+  allow_subnet_cidr_routes_overlap = true
+  network                          = google_compute_network.net-cidr-overlap.id
+}
+
+resource "google_compute_network" "net-cidr-overlap" {
+  provider                = google-beta
+
+  name                    = "net-cidr-overlap-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
