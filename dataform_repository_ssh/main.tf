b60a5abe5df6aeb6e7f361665bc11543c5898ba2
The Magician <magic-modules@google.com>
2023-10-10 19:05:59 -0400

    Dataform Repository Resource Adding requested fields (#9202) (#569)
    * Dataform Repository Resource Adding Service Account and SshAuthenticationConfig
    
    * Adding separate test exmple file and enhancing descriptions for repository resource
    
    * Adding example in repository resource
    
    * Fixing test for Dataform Repository resource
    [upstream:95fe4c502e9f9aba11f71ec9c17ce1998cedbac8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dataform_repository_ssh/main.tf
@@ -0,0 +1,42 @@
+resource "google_sourcerepo_repository" "git_repository" {
+  provider = google-beta
+  name = "my/repository-${local.name_suffix}"
+}
+
+resource "google_secret_manager_secret" "secret" {
+  provider = google-beta
+  secret_id = "secret"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret_version" {
+  provider = google-beta
+  secret = google_secret_manager_secret.secret.id
+
+  secret_data = "secret-data-${local.name_suffix}"
+}
+
+resource "google_dataform_repository" "dataform_respository" {
+  provider = google-beta
+  name = "dataform_repository-${local.name_suffix}"
+
+  git_remote_settings {
+      url = google_sourcerepo_repository.git_repository.url
+      default_branch = "main"
+      ssh_authentication_config {
+        user_private_key_secret_version = google_secret_manager_secret_version.secret_version.id
+        host_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSU"
+      }
+  }
+
+  workspace_compilation_overrides {
+    default_database = "database"
+    schema_suffix = "_suffix"
+    table_prefix = "prefix_"
+  }
+
+  service_account = "1234567890-compute@developer.gserviceaccount.com"
+}
