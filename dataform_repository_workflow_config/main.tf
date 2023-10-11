429d691ad8887ece375fc51baad8a6b1d1d10fee
The Magician <magic-modules@google.com>
2023-10-11 16:48:42 -0400

    Make service account name sweepable and unique in acc test (#9226) (#571)
    * Make service account name non-static
    
    * Update WorkflowConfig.yaml
    [upstream:45d8023ab569afaedfb92df9a92a0436162cc6c2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- dataform_repository_workflow_config/main.tf
+++ dataform_repository_workflow_config/main.tf
@@ -65,7 +65,7 @@ resource "google_dataform_repository_release_config" "release_config" {
 
 resource "google_service_account" "dataform_sa" {
   provider     = google-beta
-  account_id   = "dataform-workflow-sa"
+  account_id   = "dataform-sa-${local.name_suffix}"
   display_name = "Dataform Service Account"
 }
 
