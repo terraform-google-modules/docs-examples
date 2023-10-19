bdfbeb8cb524d6eac29dc2b37cac998a683a5a09
Modular Magician <magic-modules@google.com>
2023-10-19 19:44:20 +0000

    Adding settings to Dialogflow CX resources (#9078) [upstream:465cfd537dbbb13854796e5954994c4c4a3c92e6]
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- dialogflowcx_agent_full/main.tf
+++ dialogflowcx_agent_full/main.tf
@@ -1,3 +1,9 @@
+resource "google_storage_bucket" "bucket" {
+  name                        = "dialogflowcx-bucket-${local.name_suffix}"
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
 resource "google_dialogflow_cx_agent" "full_agent" {
   display_name = "dialogflowcx-agent-${local.name_suffix}"
   location = "global"
@@ -8,7 +14,40 @@ resource "google_dialogflow_cx_agent" "full_agent" {
   avatar_uri = "https://cloud.google.com/_static/images/cloud/icons/favicons/onecloud/super_cloud.png"
   enable_stackdriver_logging = true
   enable_spell_correction    = true
-	speech_to_text_settings {
-		enable_speech_adaptation = true
-	}
+  speech_to_text_settings {
+    enable_speech_adaptation = true
+  }
+  advanced_settings {
+    audio_export_gcs_destination {
+      uri = "${google_storage_bucket.bucket.url}/prefix-"
+    }
+    dtmf_settings {
+      enabled = true
+      max_digits = 1
+      finish_digit = "#"
+    }
+  }
+  git_integration_settings {
+    github_settings {
+      display_name = "Github Repo"
+      repository_uri = "https://api.github.com/repos/githubtraining/hellogitworld"
+      tracking_branch = "main"
+      access_token = "secret-token"
+      branches = ["main"]
+    }
+  }
+  text_to_speech_settings {
+    synthesize_speech_configs = jsonencode({
+      en = {
+        voice = {
+          name = "en-US-Neural2-A"
+        }
+      }
+      fr = {
+        voice = {
+          name = "fr-CA-Neural2-A",
+        }
+      }
+    })
+  }
 }
