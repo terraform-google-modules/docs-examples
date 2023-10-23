aadf15d20477324b3763799f2a06aa698cd5be13
The Magician <magic-modules@google.com>
2023-10-23 06:14:40 -0700

    Add remaining fields to TPU VM resource (#9308) (#580)
    * Revert "Skip generated tests for google_tpu_v2_vm (#9315)"
    
    This reverts commit 5dc9f2ea88610c0d2a2a94d04548d60e90e1cc54.
    
    * Add remaining fields to TPU VM resource
    
    * Change update test to use preemptible vm
    
    * Retain disk in test so that it isn't removed prematurely
    
    * Add networking fields
    
    * Respond to PR comments
    
    * More tweaks from PR comments
    
    * Tweak test names based on feedback
    [upstream:b48fc2d24968dc972360eb82c704b4eaeb8b40ba]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tpu_v2_vm_full/main.tf
+++ tpu_v2_vm_full/main.tf
@@ -15,4 +15,76 @@ resource "google_tpu_v2_vm" "tpu" {
 
   runtime_version  = "tpu-vm-tf-2.13.0"
   accelerator_type = "v2-8"
+
+  cidr_block = "10.0.0.0/29"
+
+  network_config {
+    can_ip_forward      = true
+    enable_external_ips = true
+    network             = google_compute_network.network.id
+    subnetwork          = google_compute_subnetwork.subnet.id
+  }
+  
+  scheduling_config {
+    preemptible = true
+  }
+
+  shielded_instance_config {
+    enable_secure_boot = true
+  }
+
+  service_account {
+    email = google_service_account.sa.email
+    scope = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  data_disks {
+    source_disk = google_compute_disk.disk.id
+    mode        = "READ_ONLY"
+  }
+
+  labels = {
+    foo = "bar"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  tags = ["foo"]
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  provider = google-beta
+
+  name          = "tpu-subnet-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.network.id
+}
+
+resource "google_compute_network" "network" {
+  provider = google-beta
+
+  name                    = "tpu-net-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_service_account" "sa" {
+  provider = google-beta
+
+  account_id   = "tpu-sa-${local.name_suffix}"
+  display_name = "Test TPU VM"
+}
+
+resource "google_compute_disk" "disk" {
+  provider = google-beta
+
+  name  = "tpu-disk-${local.name_suffix}"
+  image = "debian-cloud/debian-11"
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-c"
 }
