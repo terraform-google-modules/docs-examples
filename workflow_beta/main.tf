7bcea44a433e2ba6ce14b2cc2cab24693963dace
The Magician <magic-modules@google.com>
2023-10-25 07:52:51 -0700

    feat(workflows): add user_env_vars field + tests (#9342) (#584)
    [upstream:c7d666dda8261136feeb282fdd84c8fad3f586ce]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ workflow_beta/main.tf
@@ -0,0 +1,47 @@
+resource "google_service_account" "test_account" {
+  provider     = google-beta
+  account_id   = "my-account-${local.name_suffix}"
+  display_name = "Test Service Account"
+}
+
+resource "google_workflows_workflow" "example_beta" {
+  provider      = google-beta
+  name          = "workflow_beta-${local.name_suffix}"
+  region        = "us-central1"
+  description   = "Magic"
+  service_account = google_service_account.test_account.id
+  labels = {
+    env = "test"
+  }
+  user_env_vars = {
+    foo = "BAR"
+  }
+  source_contents = <<-EOF
+  # This is a sample workflow. You can replace it with your source code.
+  #
+  # This workflow does the following:
+  # - reads current time and date information from an external API and stores
+  #   the response in currentTime variable
+  # - retrieves a list of Wikipedia articles related to the day of the week
+  #   from currentTime
+  # - returns the list of articles as an output of the workflow
+  #
+  # Note: In Terraform you need to escape the $$ or it will cause errors.
+
+  - getCurrentTime:
+      call: http.get
+      args:
+          url: https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam
+      result: currentTime
+  - readWikipedia:
+      call: http.get
+      args:
+          url: https://en.wikipedia.org/w/api.php
+          query:
+              action: opensearch
+              search: $${currentTime.body.dayOfWeek}
+      result: wikiResult
+  - returnOutput:
+      return: $${wikiResult.body[1]}
+EOF
+}
