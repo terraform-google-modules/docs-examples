19a960f3d8621a265cf70007f7fcdb44e728d9c8
The Magician <magic-modules@google.com>
2023-11-30 08:57:14 -0800

    Add Secure Source Manager product and Instance resource (#9415) (#608)
    * Add ssm product and instance.yaml
    
    * Add instance id parameter and keep name as output property
    
    * move to beta
    
    * Add createTime, updateTime, state fields
    
    * Add basic instance test
    
    * Remove blank lines
    
    * Add one blank line back
    
    * Move back to GA provider
    
    * Fix iam policy
    
    * move iam definition up
    
    * Add iam roles
    
    * Add iam tests
    
    * Add test to examples
    
    * Remove iam policy, adjust context and variable names
    
    * Revert "Remove iam policy, adjust context and variable names"
    
    This reverts commit 8e99a3997af8d96ebe58dc15e6d614ff5f245ae2.
    
    * remove iam policy, use ctx var for instance id
    
    * Use format with random suffix
    
    * Slight change import_format for iam, remove provider explicit
    
    * Fix instance_id in generated iam test
    
    * Remove iam test
    
    * Remove reference to the iam test
    
    * edit to test again
    
    * add iam conditions to test
    
    * Update import format
    
    * remove iam condition test
    
    * revert import format
    
    * Fix links
    
    * Remove update fields from resource, remove enum values for output field, add iam_conditions_request_type
    
    * Add back values:
    
    * Add new line
    
    * add unknown value back
    
    * Remove iam_conditions_request_type
    [upstream:e06981ab153016b6c49db1fd134685fc09d9a71b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secure_source_manager_instance_basic/main.tf
@@ -0,0 +1,7 @@
+resource "google_secure_source_manager_instance" "default" {
+    location = "us-central1"
+    instance_id = "my-instance-${local.name_suffix}"
+    labels = {
+      "foo" = "bar"
+    }
+}
