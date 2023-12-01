5674451af199fc5bddfe675eacea7d97411ae346
The Magician <magic-modules@google.com>
2023-11-30 18:00:53 -0800

    fixed artifact_registry_repository permadiff in unordered array (#9376) (#609)
    * fixed permadiff in unordered array
    
    * changed permadiff fix strategy for upstream policies
    
    * removed no-op field
    
    * change diff semantics to use schema.Set
    
    * updated virtual example with intentional out-of-order priorities
    
    * update virtual policy comparison to not panic on bad cast (if possible)
    [upstream:9988136d1c8e3ef6dfb0506fe78fc07cd4a0097f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- artifact_registry_repository_virtual/main.tf
+++ artifact_registry_repository_virtual/main.tf
@@ -1,7 +1,14 @@
-resource "google_artifact_registry_repository" "my-repo-upstream" {
+resource "google_artifact_registry_repository" "my-repo-upstream-1" {
   location      = "us-central1"
-  repository_id = "my-repository-upstream-${local.name_suffix}"
-  description   = "example docker repository (upstream source)-${local.name_suffix}"
+  repository_id = "my-repository-upstream-${local.name_suffix}-1"
+  description   = "example docker repository (upstream source)-${local.name_suffix} 1"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "my-repo-upstream-2" {
+  location      = "us-central1"
+  repository_id = "my-repository-upstream-${local.name_suffix}-2"
+  description   = "example docker repository (upstream source)-${local.name_suffix} 2"
   format        = "DOCKER"
 }
 
@@ -14,9 +21,14 @@ resource "google_artifact_registry_repository" "my-repo" {
   mode          = "VIRTUAL_REPOSITORY"
   virtual_repository_config {
     upstream_policies {
-      id          = "my-repository-upstream-${local.name_suffix}"
-      repository  = google_artifact_registry_repository.my-repo-upstream.id
-      priority    = 1
+      id          = "my-repository-upstream-${local.name_suffix}-1"
+      repository  = google_artifact_registry_repository.my-repo-upstream-1.id
+      priority    = 20
+    }
+    upstream_policies {
+      id          = "my-repository-upstream-${local.name_suffix}-2"
+      repository  = google_artifact_registry_repository.my-repo-upstream-2.id
+      priority    = 10
     }
   }
 }
