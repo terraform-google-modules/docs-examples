eca582c2966d61a305a28583edfc1202d2bb04ea
The Magician <magic-modules@google.com>
2023-12-04 13:51:49 -0800

    Cloud Run v2 limits examples and docs (#9563) (#610)
    * Cloud Run v2 limits examples and docs
    
    Improve documentation for limits and provide examples how they can be
    used in Cloud Run Service and Job.
    
    * Fix limits declaration
    
    * Remove primary_resource_name
    [upstream:3005b4c8c20569581358b99ab7b77aefe485fcc3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_service_limits/main.tf
@@ -0,0 +1,17 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service-${local.name_suffix}"
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+}
