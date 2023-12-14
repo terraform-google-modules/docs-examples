52a92696cabcccef2240ff91ae5e999e0e395235
Modular Magician <magic-modules@google.com>
2023-12-14 23:53:50 +0000

    Replace "Pubsub Subscription Different Project" example with making all examples cross-project-friendly (#9637)
    * Fix "Pubsub Subscription Different Project" example
    
    PR #2342 changed this example from using `id` to `name`, which removed the project ID from the topic. However, the point of this example is to show how to set up a cross-project subscription, so the example no longer works. This change reverts that change from PR #2342.
    
    Fixes hashicorp/terraform-provider-google#11642.
    Fixes hashicorp/terraform-provider-google#6024.
    
    * Make all examples cross-project-friendly and update topic description
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    [upstream:b3ae8dcd7e476f796a8b1ba67ce63cea711be127]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- pubsub_subscription_dead_letter/main.tf
+++ pubsub_subscription_dead_letter/main.tf
@@ -8,7 +8,7 @@ resource "google_pubsub_topic" "example_dead_letter" {
 
 resource "google_pubsub_subscription" "example" {
   name  = "example-subscription-${local.name_suffix}"
-  topic = google_pubsub_topic.example.name
+  topic = google_pubsub_topic.example.id
 
   dead_letter_policy {
     dead_letter_topic = google_pubsub_topic.example_dead_letter.id
--- pubsub_subscription_push_bq/main.tf
+++ pubsub_subscription_push_bq/main.tf
@@ -4,7 +4,7 @@ resource "google_pubsub_topic" "example" {
 
 resource "google_pubsub_subscription" "example" {
   name  = "example-subscription-${local.name_suffix}"
-  topic = google_pubsub_topic.example.name
+  topic = google_pubsub_topic.example.id
 
   bigquery_config {
     table = "${google_bigquery_table.test.project}.${google_bigquery_table.test.dataset_id}.${google_bigquery_table.test.table_id}"
