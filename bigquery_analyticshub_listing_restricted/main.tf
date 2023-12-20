b9205e850cdbf4dc53416c7023de51de756bc876
The Magician <magic-modules@google.com>
2023-12-20 10:47:16 -0800

    bq analyticshub listing - add support for restrictedExportConfig (#9661) (#628)
    * bq analyticshub listing - add support for restrictedExportConfig
    
    * lint
    
    * Update mmv1/products/bigqueryanalyticshub/Listing.yaml
    
    ---------
    
    
    [upstream:9baa61fae74633eaa57e63c532a0bef70c12aaa6]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_analyticshub_listing_restricted/main.tf
@@ -0,0 +1,30 @@
+resource "google_bigquery_analytics_hub_data_exchange" "listing" {
+  location         = "US"
+  data_exchange_id = "my_data_exchange-${local.name_suffix}"
+  display_name     = "my_data_exchange-${local.name_suffix}"
+  description      = "example data exchange-${local.name_suffix}"
+}
+
+resource "google_bigquery_analytics_hub_listing" "listing" {
+  location         = "US"
+  data_exchange_id = google_bigquery_analytics_hub_data_exchange.listing.data_exchange_id
+  listing_id       = "my_listing-${local.name_suffix}"
+  display_name     = "my_listing-${local.name_suffix}"
+  description      = "example data exchange-${local.name_suffix}"
+
+  bigquery_dataset {
+    dataset = google_bigquery_dataset.listing.id
+  }
+
+  restricted_export_config {
+    enabled               = true
+    restrict_query_result = true
+  }
+}
+
+resource "google_bigquery_dataset" "listing" {
+  dataset_id                  = "my_listing-${local.name_suffix}"
+  friendly_name               = "my_listing-${local.name_suffix}"
+  description                 = "example data exchange-${local.name_suffix}"
+  location                    = "US"
+}
