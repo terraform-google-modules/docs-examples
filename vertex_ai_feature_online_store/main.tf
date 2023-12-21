12ef42dfc562d95d9f9366d330a12c5ed2b6db31
The Magician <magic-modules@google.com>
2023-12-21 06:35:16 -0800

    add vertexai/featureonlinestore resource (#9632) (#629)
    * rebase featureonlinestore based on merged changes
    
    * rebase based on merged featureonlinestore pr
    
    * fix pr suggestions featureonlinestore
    
    * fix documentation and pr comments featureonlinestore
    [upstream:f6b6532046e9886815386703ebdbfc9cfbcada93]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- vertex_ai_feature_online_store/main.tf
+++ vertex_ai_feature_online_store/main.tf
@@ -1,15 +1,14 @@
-resource google_vertex_ai_feature_online_store "feature_online_store" {
-    name = "example_feature_online_store-${local.name_suffix}"
-    region = "us-central1"
-    labels = {
-        label-one = "value-one"
+resource "google_vertex_ai_feature_online_store" "feature_online_store" {
+  name = "example_feature_online_store-${local.name_suffix}"
+  labels = {
+    foo = "bar"
+  }
+  region = "us-central1"
+  bigtable {
+    auto_scaling {
+      min_node_count         = 1
+      max_node_count         = 3
+      cpu_utilization_target = 50
     }
-
-    bigtable {
-        auto_scaling {
-            min_node_count = 1
-            max_node_count = 2
-            cpu_utilization_target = 60
-        }
-    }
+  }
 }
