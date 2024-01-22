5b9a15f4125e672fdf16d9d668582b38693835bf
The Magician <magic-modules@google.com>
2024-01-22 15:03:19 -0800

    artifact registry repository - promote cleanup policy to ga (#9832) (#647)
    [upstream:d143a70d81257b3ac440de831f5608144fc4b043]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- artifact_registry_repository_cleanup/main.tf
+++ artifact_registry_repository_cleanup/main.tf
@@ -1,5 +1,4 @@
 resource "google_artifact_registry_repository" "my-repo" {
-  provider      = google-beta
   location      = "us-central1"
   repository_id = "my-repository-${local.name_suffix}"
   description   = "example docker repository with cleanup policies-${local.name_suffix}"
