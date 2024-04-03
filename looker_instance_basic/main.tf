85d9d8c24fd67343923134536d2f7831cbccbe65
Modular Magician <magic-modules@google.com>
2024-04-03 01:20:49 +0000

    Update examples to use LOOKER_CORE_STANDARD_ANNUAL edition (#10325)
    [upstream:09ade7e2a6326df0431ac2aeb4a80d151ca87a85]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- looker_instance_basic/main.tf
+++ looker_instance_basic/main.tf
@@ -1,6 +1,6 @@
 resource "google_looker_instance" "looker-instance" {
   name              = "my-instance-${local.name_suffix}"
-  platform_edition  = "LOOKER_CORE_STANDARD"
+  platform_edition  = "LOOKER_CORE_STANDARD_ANNUAL"
   region            = "us-central1"
   oauth_config {
     client_id = "my-client-id-${local.name_suffix}"
--- looker_instance_custom_domain/main.tf
+++ looker_instance_custom_domain/main.tf
@@ -1,11 +1,13 @@
 resource "google_looker_instance" "looker-instance" {
   name              = "my-instance-${local.name_suffix}"
-  platform_edition  = "LOOKER_CORE_STANDARD"
+  platform_edition  = "LOOKER_CORE_STANDARD_ANNUAL"
   region            = "us-central1"
   oauth_config {
     client_id = "my-client-id-${local.name_suffix}"
     client_secret = "my-client-secret-${local.name_suffix}"
   }
+  // After your Looker (Google Cloud core) instance has been created, you can set up, view information about, or delete a custom domain for your instance. 
+  // Therefore 2 terraform applies, one to create the instance, then another to set up the custom domain. 
   custom_domain {
     domain = "my-custom-domain-${local.name_suffix}.com"
   }
