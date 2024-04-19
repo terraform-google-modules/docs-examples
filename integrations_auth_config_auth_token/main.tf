66cbdf501c59ebd510faf4c67858566d7de87456
Modular Magician <magic-modules@google.com>
2024-04-19 17:12:17 +0000

    Add AuthConfig resource in Integrations product (#10335)
    [upstream:202f42b9e69aa5b591796d7b31121a8b3bb92eb1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ integrations_auth_config_auth_token/main.tf
@@ -0,0 +1,18 @@
+resource "google_integrations_client" "client" {
+  location = "us-west2"
+  provision_gmek = true
+}
+
+resource "google_integrations_auth_config" "auth_token_example" {
+    location = "us-west2"
+    display_name = "test-authconfig-auth-token-${local.name_suffix}"
+    description = "Test auth config created via terraform"
+    decrypted_credential {
+        credential_type = "AUTH_TOKEN"
+        auth_token {
+            type = "Basic"
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+        }
+    }
+    depends_on = [google_integrations_client.client]
+}
---
+++ integrations_auth_config_oidc_token/main.tf
@@ -0,0 +1,23 @@
+resource "google_integrations_client" "client" {
+  location = "us-south1"
+  provision_gmek = true
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = "sa-${local.name_suffix}"
+  display_name = "Service Account"
+}
+
+resource "google_integrations_auth_config" "oidc_token_example" {
+    location = "us-south1"
+    display_name = "test-authconfig-oidc-token-${local.name_suffix}"
+    description = "Test auth config created via terraform"
+    decrypted_credential {
+        credential_type = "OIDC_TOKEN"
+        oidc_token {
+            service_account_email = google_service_account.service_account.email
+            audience = "https://us-south1-project.cloudfunctions.net/functionA 1234987819200.apps.googleusercontent.com"
+        }
+    }
+    depends_on = [google_service_account.service_account, google_integrations_client.client]
+}
