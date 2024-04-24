f609fac53e5ec67816d63cf3c0c677a44812501b
Bob Furu <47002677+bobfuru@users.noreply.github.com>
2024-04-24 13:42:34 -0600

    Update service `name` and remove `ALL_TRAFFIC`
    - Sample doesn't need `ALL_TRAFFIC`
    - Name is invalid

--- cloudrunv2_service_directvpc/main.tf
+++ cloudrunv2_service_directvpc/main.tf
@@ -1,5 +1,5 @@
 resource "google_cloud_run_v2_service" "default" {
-  name     = "cloudrun-service-${local.name_suffix}"
+  name     = "cloudrun-service"
   location = "us-central1"
   launch_stage = "GA"
   template {
@@ -12,7 +12,6 @@ resource "google_cloud_run_v2_service" "default" {
         subnetwork = "default"
         tags = ["tag1", "tag2", "tag3"]
       }
-      egress = "ALL_TRAFFIC"
     }
   }
 }
