8ab0cb30cf0d75b6a22d0136e18e0766112a6b7d
The Magician <magic-modules@google.com>
2024-05-08 15:44:03 -0700

    Add KMS support for the google_bigquery_connection resource (#10561) (#706)
    [upstream:807e2f6c28607f989af9741458844a41a4ae8ffa]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_connection_kms/main.tf
@@ -0,0 +1,37 @@
+resource "google_sql_database_instance" "instance" {
+    name             = "my-database-instance-${local.name_suffix}"
+    database_version = "POSTGRES_11"
+    region           = "us-central1"
+    settings {
+		tier = "db-f1-micro"
+	}
+
+    deletion_protection  = "false"
+}
+
+resource "google_sql_database" "db" {
+    instance = google_sql_database_instance.instance.name
+    name     = "db"
+}
+
+resource "google_sql_user" "user" {
+    name = "user-${local.name_suffix}"
+    instance = google_sql_database_instance.instance.name
+    password = "tf-test-my-password%{random_suffix}"
+}
+
+resource "google_bigquery_connection" "bq-connection-cmek" {
+    friendly_name = "👋"
+    description   = "a riveting description"
+    location      = "US"
+    kms_key_name  = "projects/project/locations/us-central1/keyRings/us-central1/cryptoKeys/bq-key-${local.name_suffix}"
+    cloud_sql {
+        instance_id = google_sql_database_instance.instance.connection_name
+        database    = google_sql_database.db.name
+        type        = "POSTGRES"
+        credential {
+          username = google_sql_user.user.name
+          password = google_sql_user.user.password
+        }
+    }
+}
