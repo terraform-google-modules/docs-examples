fd70dbfed70d71d0641682ba57838dbcb21ba2ac
The Magician <magic-modules@google.com>
2024-06-12 14:37:22 -0700

    Add autoscaling to DPMS terraform (#10522) (#724)
    [upstream:6742c3a25b388ebbd567b462693c4855e5c8c179]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dataproc_metastore_service_autoscaling_min_and_max_scaling_factor/main.tf
@@ -0,0 +1,22 @@
+resource "google_dataproc_metastore_service" "test_resource" {
+  service_id = "test-service-${local.name_suffix}"
+  location   = "us-central1"
+
+  # DPMS 2 requires SPANNER database type, and does not require
+  # a maintenance window.
+  database_type = "SPANNER"
+
+  hive_metastore_config {
+    version           = "3.1.2"
+  }
+
+  scaling_config {
+    autoscaling_config {
+      autoscaling_enabled = true
+      limit_config {
+        min_scaling_factor = 0.1
+        max_scaling_factor = 1.0
+      }
+    }
+  }
+}
