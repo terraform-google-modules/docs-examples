27a6810fc2be5ef3a2534a488b99037636af3466
Modular Magician <magic-modules@google.com>
2024-06-13 17:15:55 +0000

    Fix ArtifactRegistry Yum test (#10956)
    [upstream:f105e6a3363e89404c608abd5a011ad74598609c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- artifact_registry_repository_remote_yum/main.tf
+++ artifact_registry_repository_remote_yum/main.tf
@@ -1,15 +1,15 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location      = "us-central1"
-  repository_id = "centos-8-${local.name_suffix}"
+  repository_id = "rocky-9-${local.name_suffix}"
   description   = "example remote yum repository-${local.name_suffix}"
   format        = "YUM"
   mode          = "REMOTE_REPOSITORY"
   remote_repository_config {
-    description = "Centos 8 remote repository"
+    description = "Rocky 9 remote repository"
     yum_repository {
       public_repository {
-        repository_base = "CENTOS"
-        repository_path = "centos/8-stream/BaseOS/x86_64/os"
+        repository_base = "ROCKY"
+        repository_path = "pub/rocky/9/BaseOS/x86_64/os"
       }
     }
   }
