391995e4c05d68c8ebcab0a7d2f8378ab0de11a3
The Magician <magic-modules@google.com>
2024-06-25 14:46:46 -0700

    Merge pull request #11023 from c2thorn/sync-main-FEATURE-BRANCH-6.0.0 (#733)
    Sync main feature branch 6.0.0
    
    [upstream:22e1ca94afba382c6bfd496d8d1bbfb76782182b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- network_attachment_instance_usage/main.tf
+++ network_attachment_instance_usage/main.tf
@@ -1,11 +1,9 @@
 resource "google_compute_network" "default" {
-    provider = google-beta
     name = "basic-network-${local.name_suffix}"
     auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "default" {
-    provider = google-beta
     name   = "basic-subnetwork-${local.name_suffix}"
     region = "us-central1"
 
@@ -14,7 +12,6 @@ resource "google_compute_subnetwork" "default" {
 }
 
 resource "google_compute_network_attachment" "default" {
-    provider = google-beta
     name   = "basic-network-attachment-${local.name_suffix}"
     region = "us-central1"
     description = "my basic network attachment"
@@ -24,7 +21,6 @@ resource "google_compute_network_attachment" "default" {
 }
 
 resource "google_compute_instance" "default" {
-    provider = google-beta
     name         = "basic-instance-${local.name_suffix}"
     zone         = "us-central1-a"
     machine_type = "e2-micro"
