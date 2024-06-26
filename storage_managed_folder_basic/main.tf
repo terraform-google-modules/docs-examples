b7d75718618da0d034b9ac6415ad6f84c5540ce9
Modular Magician <magic-modules@google.com>
2024-06-26 01:00:44 +0000

    Add support for GCS managed folders (#10786)
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    [upstream:0836fbe77e64a07242c695326680a72bfe3861af]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ storage_managed_folder_basic/main.tf
@@ -0,0 +1,10 @@
+resource "google_storage_bucket" "bucket" {
+  name                        = "my-bucket-${local.name_suffix}"
+  location                    = "EU"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_managed_folder" "folder" {
+  bucket = google_storage_bucket.bucket.name
+  name   = "managed/folder/name/"
+}
