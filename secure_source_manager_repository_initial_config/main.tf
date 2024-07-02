69fe102068122cced0ae7bb35248760b196c844f
Modular Magician <magic-modules@google.com>
2024-07-02 16:55:16 +0000

    Merge pull request #11088 from c2thorn/sync-main-FEATURE-BRANCH-6.0.0
    Sync main feature branch 6.0.0 - 7/1
    
    [upstream:a8fd8256a904bb9e360547dfc96a9e01a1ff9584]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secure_source_manager_repository_initial_config/main.tf
@@ -0,0 +1,18 @@
+resource "google_secure_source_manager_instance" "instance" {
+    location = "us-central1"
+    instance_id = "my-instance-${local.name_suffix}"
+}
+
+resource "google_secure_source_manager_repository" "default" {
+    location = "us-central1"
+    repository_id = "my-repository-${local.name_suffix}"
+    instance = google_secure_source_manager_instance.instance.name
+
+    description = "This is a test repository"
+    initial_config {
+      default_branch = "main"
+      gitignores = ["python"]
+      license = "mit"
+      readme = "default"
+    }
+}
