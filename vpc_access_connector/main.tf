4bbbb48e1c9cf832dcc8017bc7da0c87a47ee19e
The Magician <magic-modules@google.com>
2024-07-10 10:44:47 -0700

    Fix permadiff on google_vpc_access_connector resource with breaking changes (min_instances, max_instances, min_throughput, max_throughput fields) (#10313) (#741)
    [upstream:f8834bc45fcf94b05c0e6f7de2dcf8003e6d7745]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- vpc_access_connector/main.tf
+++ vpc_access_connector/main.tf
@@ -2,4 +2,6 @@ resource "google_vpc_access_connector" "connector" {
   name          = "vpc-con-${local.name_suffix}"
   ip_cidr_range = "10.8.0.0/28"
   network       = "default-${local.name_suffix}"
+  min_instances = 2
+  max_instances = 3
 }
