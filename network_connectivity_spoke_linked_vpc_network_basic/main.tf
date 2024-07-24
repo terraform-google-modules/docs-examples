5142ccdd86152369bf7e96ac036436a5c3aebd0a
The Magician <magic-modules@google.com>
2024-07-24 13:28:15 -0700

    Merge pull request #11237 from c2thorn/sync-main-FEATURE-BRANCH-6.0.0 (#752)
    Sync main feature branch 6.0.0 - 7/23
    
    [upstream:fa001dc2531704f19fd9302667a8a26672d9c30b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_connectivity_spoke_linked_vpc_network_basic/main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_network" "network" {
+  name                    = "net-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_network_connectivity_hub" "basic_hub" {
+  name        = "hub1-${local.name_suffix}"
+  description = "A sample hub"
+  labels = {
+    label-two = "value-one"
+  }
+}
+
+resource "google_network_connectivity_spoke" "primary"  {
+  name = "spoke1-${local.name_suffix}"
+  location = "global"
+  description = "A sample spoke with a linked router appliance instance"
+  labels = {
+    label-one = "value-one"
+  }
+  hub = google_network_connectivity_hub.basic_hub.id
+  linked_vpc_network {
+    exclude_export_ranges = [
+      "198.51.100.0/24",
+      "10.10.0.0/16"
+    ]
+    uri = google_compute_network.network.self_link
+  }
+}
