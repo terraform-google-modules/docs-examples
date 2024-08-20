a00807efa54b7f22668c3b7c8d6e34a9b2ab78a1
Modular Magician <magic-modules@google.com>
2024-08-20 23:22:51 +0000

    Merge pull request #11499 from c2thorn/sync-main-FEATURE-BRANCH-6.0.0
    Sync main feature branch 6.0.0 - 8/20
    
    [upstream:5e11f505141cbb2581f1c5cf330af01e3ad0465b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ alloydb_instance_psc_test/main.tf
@@ -0,0 +1,21 @@
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "alloydb-instance-${local.name_suffix}"
+  instance_type = "PRIMARY"
+
+  machine_config {
+    cpu_count = 2
+  }
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "alloydb-cluster-${local.name_suffix}"
+  location   = "us-central1"
+
+  initial_user {
+    password = "alloydb-cluster-${local.name_suffix}"
+  }
+  psc_config {
+    psc_enabled = true
+  }
+}
