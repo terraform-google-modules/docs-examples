d4a21d6ee764a85e494969ead813187ce417341a
The Magician <magic-modules@google.com>
2024-08-27 15:02:22 -0700

    Promote Client Tls Policy to V1 (#11547) (#778)
    [upstream:61f619ede9c1e30516810a5d6799fbfe29cd91da]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- network_security_client_tls_policy_basic/main.tf
+++ network_security_client_tls_policy_basic/main.tf
@@ -1,5 +1,4 @@
 resource "google_network_security_client_tls_policy" "default" {
-  provider               = google-beta
   name                   = "my-client-tls-policy-${local.name_suffix}"
   labels                 = {
     foo = "bar"
