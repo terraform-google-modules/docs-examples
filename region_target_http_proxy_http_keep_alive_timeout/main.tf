4b9935cc62facf1df8203a6b012a798bbefc53ec
The Magician <magic-modules@google.com>
2024-09-10 09:24:59 -0700

    Add field httpKeepAliveTimeoutSec to resources google_compute_region_target_http_proxy and google_compute_region_target_https_proxy (#11634) (#789)
    [upstream:9b8f317ddadc86b5fc6d56060cb8b5fbf7324b8f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_target_http_proxy_http_keep_alive_timeout/main.tf
@@ -0,0 +1,47 @@
+resource "google_compute_region_target_http_proxy" "default" {
+  region                      = "us-central1"
+  name                        = "test-http-keep-alive-timeout-proxy-${local.name_suffix}"
+  http_keep_alive_timeout_sec = 600
+  url_map                     = google_compute_region_url_map.default.id
+}
+
+resource "google_compute_region_url_map" "default" {
+  region          = "us-central1"
+  name            = "url-map-${local.name_suffix}"
+  default_service = google_compute_region_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_region_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_region_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_region_backend_service" "default" {
+  region                = "us-central1"
+  name                  = "backend-service-${local.name_suffix}"
+  port_name             = "http"
+  protocol              = "HTTP"
+  timeout_sec           = 10
+  load_balancing_scheme = "INTERNAL_MANAGED"
+
+  health_checks = [google_compute_region_health_check.default.id]
+}
+
+resource "google_compute_region_health_check" "default" {
+  region = "us-central1"
+  name   = "http-health-check-${local.name_suffix}"
+
+  http_health_check {
+    port = 80
+  }
+}
