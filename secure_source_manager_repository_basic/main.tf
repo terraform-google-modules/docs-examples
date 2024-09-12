ea22a9437f7561ceaa2c07d2d33b43915233bd7c
Modular Magician <magic-modules@google.com>
2024-09-12 19:41:06 +0000

    Add lifecycle prevent destroy to Secure Source Manager examples (#11691)
    [upstream:500d423479c90d7b3108dcda3dfbdd80d98c707a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- secure_source_manager_repository_basic/main.tf
+++ secure_source_manager_repository_basic/main.tf
@@ -1,10 +1,20 @@
 resource "google_secure_source_manager_instance" "instance" {
     location = "us-central1"
     instance_id = "my-instance-${local.name_suffix}"
+
+    # Prevent accidental deletions.
+    lifecycle {
+      prevent_destroy = "false"
+    }
 }
 
 resource "google_secure_source_manager_repository" "default" {
     location = "us-central1"
     repository_id = "my-repository-${local.name_suffix}"
     instance = google_secure_source_manager_instance.instance.name
+
+    # Prevent accidental deletions.
+    lifecycle {
+      prevent_destroy = "false"
+    }
 }
