7d3fbadaf28ea4ffc711d7e1f97fe21824f1ded4
The Magician <magic-modules@google.com>
2024-09-17 09:46:29 -0700

    Metastore: add `deletion_protection` (#11731) (#798)
    [upstream:6bf739694b6e14a83c0c057a154a4ba3c6489ec0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dataproc_metastore_service_deletion_protection/main.tf
@@ -0,0 +1,21 @@
+resource "google_dataproc_metastore_service" "default" {
+    service_id          = "metastore-srv-${local.name_suffix}"
+    location            = "us-central1"
+    port                = 9080
+    tier                = "DEVELOPER"
+    deletion_protection = "false"
+  
+    maintenance_window {
+      hour_of_day = 2
+      day_of_week = "SUNDAY"
+    }
+  
+    hive_metastore_config {
+      version = "2.3.6"
+    }
+  
+    labels = {
+      env = "test"
+    }
+  }
+  
