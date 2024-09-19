b9bbbcd93dd3d9dfcb027f60e40eb61f7ac19960
Modular Magician <magic-modules@google.com>
2024-09-19 21:05:20 +0000

    Add new resource for creating Whistle Mapping, Reconciliation and Backfill Pipeline Jobs for Healthcare Data Engine  (#11677)
    [upstream:1902bacf8e9c72befb6dfb29945bc5effb7355ab]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ healthcare_pipeline_job_backfill/main.tf
@@ -0,0 +1,13 @@
+resource "google_healthcare_pipeline_job" "example-pipeline" {
+  name  = "example_backfill_pipeline-${local.name_suffix}"
+  location = "us-central1"
+  dataset = google_healthcare_dataset.dataset.id
+  backfill_pipeline_job {
+    mapping_pipeline_job = "${google_healthcare_dataset.dataset.id}/pipelinejobs/example_mapping_pipeline-${local.name_suffix}"
+  }      
+}
+
+resource "google_healthcare_dataset" "dataset" {
+  name     = "example_dataset-${local.name_suffix}"
+  location = "us-central1"
+}
