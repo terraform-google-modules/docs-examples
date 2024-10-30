43bc0c5855b72a42c7d1a6bbcc26d6051045683e
Modular Magician <magic-modules@google.com>
2024-10-30 18:01:38 +0000

    Promoting Memorystore to GA (#12112)
    [upstream:61370a87fcc2c9f8f9040b692b728c6ba20077e4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ memorystore_instance_persistence_aof/main.tf
@@ -0,0 +1,48 @@
+resource "google_memorystore_instance" "instance-persistence-aof" {
+  instance_id = "aof-instance-${local.name_suffix}"
+  shard_count = 3
+  desired_psc_auto_connections {
+    network    = google_compute_network.producer_net.id
+    project_id = data.google_project.project.project_id
+  }
+  location = "us-central1"
+  persistence_config {
+    mode = "AOF"
+    aof_config {
+      append_fsync = "EVERY_SEC"
+    }
+  }
+  depends_on = [
+    google_network_connectivity_service_connection_policy.default
+  ]
+  deletion_protection_enabled = false
+  lifecycle {
+    prevent_destroy = "true-${local.name_suffix}"
+  }
+}
+
+resource "google_network_connectivity_service_connection_policy" "default" {
+  name          = "my-policy-${local.name_suffix}"
+  location      = "us-central1"
+  service_class = "gcp-memorystore"
+  description   = "my basic service connection policy"
+  network       = google_compute_network.producer_net.id
+  psc_config {
+    subnetworks = [google_compute_subnetwork.producer_subnet.id]
+  }
+}
+
+resource "google_compute_subnetwork" "producer_subnet" {
+  name          = "my-subnet-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.248/29"
+  region        = "us-central1"
+  network       = google_compute_network.producer_net.id
+}
+
+resource "google_compute_network" "producer_net" {
+  name                    = "my-network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+data "google_project" "project" {
+}
