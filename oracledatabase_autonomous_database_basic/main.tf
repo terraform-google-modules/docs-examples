524de95a4e2345327ab6127cb8fdb40cebe26797
Modular Magician <magic-modules@google.com>
2024-12-07 08:11:42 +0000

    Make google_oracle_database_autonomous_database.name field unique per acceptance test (#12511)
    [upstream:35f6ab5527f0545b71e27e0e5dc124c24890560b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- oracledatabase_autonomous_database_basic/main.tf
+++ oracledatabase_autonomous_database_basic/main.tf
@@ -2,7 +2,7 @@ resource "google_oracle_database_autonomous_database" "myADB"{
   autonomous_database_id = "my-instance-${local.name_suffix}"
   location = "us-east4"
   project = "my-project-${local.name_suffix}"
-  database = "testdb"
+  database = "mydatabase-${local.name_suffix}"
   admin_password = "123Abpassword"
   network = data.google_compute_network.default.id
   cidr = "10.5.0.0/24"
