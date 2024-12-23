392b4b3f2c3cb400a392e7d01dadd18b691a812c
Modular Magician <magic-modules@google.com>
2024-12-23 18:22:10 +0000

    Update tests to use bootstrapped KMS keys (#12609)
    [upstream:0974a52691872c92d017186d92f29420f2b3bdf1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- kmsConfig_create/main.tf
+++ kmsConfig_create/main.tf
@@ -1,17 +1,6 @@
-resource "google_kms_key_ring" "keyring" {
-  name     = "key-ring-${local.name_suffix}"
-  location = "us-central1"
-}
-
-resource "google_kms_crypto_key" "crypto_key" {
-  name            = "crypto-name-${local.name_suffix}"
-  key_ring        = google_kms_key_ring.keyring.id
-  # rotation_period = "7776000s"
-}
-
 resource "google_netapp_kmsconfig" "kmsConfig" {
   name = "kms-test-${local.name_suffix}"
   description="this is a test description"
-  crypto_key_name=google_kms_crypto_key.crypto_key.id
+  crypto_key_name="crypto-name-${local.name_suffix}"
   location="us-central1"
 }
