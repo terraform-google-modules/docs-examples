e330ccb96c2656c2081cb3255a2b728d9921caa4
Modular Magician <magic-modules@google.com>
2025-02-14 17:51:32 +0000

    Add CMEK support for Redis cluster (#12999)
    Co-authored-by: Stephen Lewis (Burrows) <stephen.r.burrows@gmail.com>
    
    [upstream:57616ae8d7af81cdd4857b86e6e50797a3420674]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- redis_cluster_rdb/main.tf
+++ redis_cluster_rdb/main.tf
@@ -2,7 +2,7 @@ resource "google_redis_cluster" "cluster-rdb" {
   name           = "rdb-cluster-${local.name_suffix}"
   shard_count    = 3
   psc_configs {
-    network = google_compute_network.producer_net.id
+    network = google_compute_network.consumer_net.id
   }
   region = "us-central1"
   replica_count = 0
@@ -41,24 +41,24 @@ resource "google_redis_cluster" "cluster-rdb" {
 }
 
 resource "google_network_connectivity_service_connection_policy" "default" {
-  name = "mypolicy-${local.name_suffix}"
+  name = "my-policy-${local.name_suffix}"
   location = "us-central1"
   service_class = "gcp-memorystore-redis"
   description   = "my basic service connection policy"
-  network = google_compute_network.producer_net.id
+  network = google_compute_network.consumer_net.id
   psc_config {
-    subnetworks = [google_compute_subnetwork.producer_subnet.id]
+    subnetworks = [google_compute_subnetwork.consumer_subnet.id]
   }
 }
 
-resource "google_compute_subnetwork" "producer_subnet" {
-  name          = "mysubnet-${local.name_suffix}"
+resource "google_compute_subnetwork" "consumer_subnet" {
+  name          = "my-subnet-${local.name_suffix}"
   ip_cidr_range = "10.0.0.248/29"
   region        = "us-central1"
-  network       = google_compute_network.producer_net.id
+  network       = google_compute_network.consumer_net.id
 }
 
-resource "google_compute_network" "producer_net" {
-  name                    = "mynetwork-${local.name_suffix}"
+resource "google_compute_network" "consumer_net" {
+  name                    = "my-network-${local.name_suffix}"
   auto_create_subnetworks = false
 }
