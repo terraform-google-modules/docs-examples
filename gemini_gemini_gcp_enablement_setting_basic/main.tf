6428fbf4b72526bcb90c506dfb1f6464e1ab0bbc
Modular Magician <magic-modules@google.com>
2025-02-21 20:25:22 +0000

    Add Gemini Gemini Gcp Enablement Setting Binding resource (#12897)
    [upstream:428e9f6830e29bf9e3468c4ee6f4889f59169a90]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- gemini_gemini_gcp_enablement_setting_basic/main.tf
+++ gemini_gemini_gcp_enablement_setting_basic/main.tf
@@ -1,5 +1,6 @@
 resource "google_gemini_gemini_gcp_enablement_setting" "example" {
     gemini_gcp_enablement_setting_id = "ls1-tf-${local.name_suffix}"
     location = "global"
+    labels = {"my_key": "my_value"}
     enable_customer_data_sharing = true
 }
