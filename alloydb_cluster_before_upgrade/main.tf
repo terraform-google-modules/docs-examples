760762ea74e5c531818123eaa08293ff429b2770
Modular Magician <magic-modules@google.com>
2025-02-26 20:00:46 +0000

    Add examples for AlloyDB Major Version Upgrade (#13177)
    Co-authored-by: Zhenhua Li <zhenhuali@google.com>
    
    [upstream:e98212e5d8fe8ac0e028a4be9b444a3dcbb6bcde]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ alloydb_cluster_before_upgrade/main.tf
@@ -0,0 +1,44 @@
+resource "google_alloydb_instance" "default" {
+  cluster       = google_alloydb_cluster.default.name
+  instance_id   = "alloydb-instance-${local.name_suffix}"
+  instance_type = "PRIMARY"
+
+  machine_config {
+    cpu_count = 2
+  }
+
+  depends_on = [google_service_networking_connection.vpc_connection]
+}
+
+resource "google_alloydb_cluster" "default" {
+  cluster_id = "alloydb-cluster-${local.name_suffix}"
+  location   = "us-central1"
+  network_config {
+    network = google_compute_network.default.id
+  }
+  database_version = "POSTGRES_14"
+
+  initial_user {
+    password = "alloydb-cluster-${local.name_suffix}"
+  }
+}
+
+data "google_project" "project" {}
+
+resource "google_compute_network" "default" {
+  name = "alloydb-network-${local.name_suffix}"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          =  "alloydb-cluster-${local.name_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  prefix_length = 16
+  network       = google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "vpc_connection" {
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+}
