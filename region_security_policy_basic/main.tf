b2082c40da54f87738d55dcd032d147e255d3753
Modular Magician <magic-modules@google.com>
2025-04-21 21:58:22 +0000

    Bump RegionSecurityPolicy and RegionSecurityPolicyRule resources to GA (#13592)
    [upstream:045a8b725e49e6d37302afa11a2c11a62d9ae978]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- region_security_policy_basic/main.tf
+++ region_security_policy_basic/main.tf
@@ -1,6 +1,4 @@
 resource "google_compute_region_security_policy" "region-sec-policy-basic" {
-  provider    = google-beta
-
   name        = "my-sec-policy-basic-${local.name_suffix}"
   description = "basic region security policy"
   type        = "CLOUD_ARMOR"
--- region_security_policy_rule_default_rule/main.tf
+++ region_security_policy_rule_default_rule/main.tf
@@ -1,8 +1,22 @@
 resource "google_compute_region_security_policy" "default" {
   region      = "us-west2"
-  name        = "policyruletest-${local.name_suffix}"
+  name        = "policywithdefaultrule-${local.name_suffix}"
   description = "basic region security policy"
   type        = "CLOUD_ARMOR"
+}
+
+resource "google_compute_region_security_policy_rule" "default_rule" {
+  region          = "us-west2"
+  security_policy = google_compute_region_security_policy.default.name
+  description     = "new rule"
+  action          = "deny"
+  priority        = "2147483647"
+  match {
+    versioned_expr = "SRC_IPS_V1"
+    config {
+      src_ip_ranges = ["*"]
+    }
+  }
 }
 
 resource "google_compute_region_security_policy_rule" "policy_rule" {
