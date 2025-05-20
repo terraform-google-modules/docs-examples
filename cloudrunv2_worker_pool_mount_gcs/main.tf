77ec385677887316a67d8d675e2c26eac10e0f26
Modular Magician <magic-modules@google.com>
2025-05-20 22:09:10 +0000

    V2 worker pool (#13816)
    [upstream:c7332d1e475b92f3ba02d7adf282f96b3c5daa2f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_worker_pool_mount_gcs/main.tf
@@ -0,0 +1,31 @@
+resource "google_cloud_run_v2_worker_pool" "default" {
+  name     = "cloudrun-worker-pool-${local.name_suffix}"
+
+  location     = "us-central1"
+  deletion_protection = false
+  launch_stage = "ALPHA"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/worker-pool"
+      volume_mounts {
+        name       = "bucket"
+        mount_path = "/var/www"
+      }
+    }
+
+    volumes {
+      name = "bucket"
+      gcs {
+        bucket    = google_storage_bucket.default.name
+        read_only = false
+      }
+    }
+  }
+}
+
+resource "google_storage_bucket" "default" {
+    name     = "cloudrun-worker-pool-${local.name_suffix}"
+    location = "US"
+    uniform_bucket_level_access = true
+}
