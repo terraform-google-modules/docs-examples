a63bf764768e517a0bee9f7ec4ab81e9b3f937fb
The Magician <magic-modules@google.com>
2025-08-25 14:40:09 -0700

    Sync feature branch resource identity generation (#14963) (#1103)
    [upstream:1bd4eb0f09711c6efab2f684ef77114f48a017ae]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secure_source_manager_instance_private_trusted_cert/main.tf
@@ -0,0 +1,8 @@
+resource "google_secure_source_manager_instance" "default" {
+  instance_id = "my-instance-${local.name_suffix}"
+  location    = "us-central1"
+
+  private_config {
+    is_private = true
+  }
+}
