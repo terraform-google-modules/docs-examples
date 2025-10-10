88e87a71c0df11698d0a26d0840751b04b816cd9
Modular Magician <magic-modules@google.com>
2025-10-10 18:42:59 +0000

    Add deletion_protection field to Redis Instance (#15024)
    [upstream:be3e073417e29bc23b6a27efc2c38998daf5cba2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- redis_instance_basic/main.tf
+++ redis_instance_basic/main.tf
@@ -1,6 +1,7 @@
 resource "google_redis_instance" "cache" {
   name           = "memory-cache-${local.name_suffix}"
   memory_size_gb = 1
+  deletion_protection = false
 
   lifecycle {
     prevent_destroy = false
