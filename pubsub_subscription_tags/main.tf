684b03bdf9fbd652265d3fd1b3a40cf9aa492e69
Modular Magician <magic-modules@google.com>
2025-10-28 22:21:24 +0000

    Sync main oct 27 feature branch resource identity (#15430)
    [upstream:00f4e1823a218ef664966a3034e73189186cdd15]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ pubsub_subscription_tags/main.tf
@@ -0,0 +1,23 @@
+resource "google_pubsub_topic" "example" {
+  name = "example-topic-${local.name_suffix}"
+}
+
+resource "google_pubsub_subscription" "example" {
+  name  = "example-subscription-${local.name_suffix}"
+  topic = google_pubsub_topic.example.id
+  tags = {
+    (google_tags_tag_key.tag_key.namespaced_name) = google_tags_tag_value.tag_value.short_name
+  }
+}
+
+data "google_project" "project" {}
+
+resource "google_tags_tag_key" "tag_key" {
+  parent     = data.google_project.project.id
+  short_name = "tag_key-${local.name_suffix}"
+}
+
+resource "google_tags_tag_value" "tag_value" {
+  parent     = google_tags_tag_key.tag_key.id
+  short_name = "tag_value-${local.name_suffix}"
+}
---
+++ wasm_plugin_basic_generic_repository/main.tf
@@ -0,0 +1,27 @@
+data "google_project" "project" {}
+
+resource "google_network_services_wasm_plugin" "wasm_plugin" {
+  name        = "my-wasm-plugin-${local.name_suffix}"
+  description = "my wasm plugin from a generic repository"
+
+  main_version_id = "v1"
+
+  labels = {
+    test_label =  "test_value"
+  }
+  log_config {
+    enable =  true
+    sample_rate = 1
+    min_log_level =  "WARN"
+  }
+
+  versions {
+    version_name = "v1"
+    description = "v1 version of my wasm plugin"
+    image_uri = "projects/${data.google_project.project.name}/locations/us-central1/repositories/my-artifact-registry-generic-repository-${local.name_suffix}/genericArtifacts/my-wasm-plugin-data-${local.name_suffix}:v1"
+
+    labels = {
+      test_label =  "test_value"
+    }
+  }
+}
