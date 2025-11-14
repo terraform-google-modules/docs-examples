4016b4466348d807f4b958e58aa05e732eacb112
Modular Magician <magic-modules@google.com>
2025-11-14 16:24:04 +0000

    Remove launch stage from Cloud Run GPU job test/example (#15527)
    [upstream:6fd16e289a3bedd08c35958075945b7492d67a29]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- cloudrunv2_job_gpu/main.tf
+++ cloudrunv2_job_gpu/main.tf
@@ -2,7 +2,6 @@ resource "google_cloud_run_v2_job" "default" {
   name     = "cloudrun-job-${local.name_suffix}"
   location = "us-central1"
   deletion_protection = false
-  launch_stage = "BETA"
   template {
     template {
       containers {
