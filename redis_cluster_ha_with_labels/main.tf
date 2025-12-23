32449997e6e6d40a0692f65636df90d1aa7d4168
The Magician <magic-modules@google.com>
2025-12-23 12:28:40 -0800

    Add support for labels for Memorystore Redis clusters (#15991) (#1194)
    [upstream:93705bca8d1c02aeb22fac8b4325e3b18ce9ae5b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ redis_cluster_ha_with_labels/main.tf
@@ -0,0 +1,61 @@
+resource "google_redis_cluster" "cluster-ha-with-labels" {
+  name           = "ha-cluster-${local.name_suffix}"
+  shard_count    = 3
+  labels = {
+    my_key = "my_val"
+    other_key = "other_val" 
+  }
+  psc_configs {
+    network = google_compute_network.consumer_net.id
+  }
+  region = "us-central1"
+  replica_count = 1
+  node_type = "REDIS_SHARED_CORE_NANO"
+  transit_encryption_mode = "TRANSIT_ENCRYPTION_MODE_DISABLED"
+  authorization_mode = "AUTH_MODE_DISABLED"
+  redis_configs = {
+    maxmemory-policy	= "volatile-ttl"
+  }
+  deletion_protection_enabled = false
+
+  zone_distribution_config {
+    mode = "MULTI_ZONE"
+  }
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = "MONDAY"
+      start_time {
+        hours = 1
+        minutes = 0
+        seconds = 0
+        nanos = 0
+      }
+    }
+  }
+  depends_on = [
+    google_network_connectivity_service_connection_policy.default
+  ]
+}
+
+resource "google_network_connectivity_service_connection_policy" "default" {
+  name = "my-policy-${local.name_suffix}"
+  location = "us-central1"
+  service_class = "gcp-memorystore-redis"
+  description   = "my basic service connection policy"
+  network = google_compute_network.consumer_net.id
+  psc_config {
+    subnetworks = [google_compute_subnetwork.consumer_subnet.id]
+  }
+}
+
+resource "google_compute_subnetwork" "consumer_subnet" {
+  name          = "my-subnet-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.248/29"
+  region        = "us-central1"
+  network       = google_compute_network.consumer_net.id
+}
+
+resource "google_compute_network" "consumer_net" {
+  name                    = "my-network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
