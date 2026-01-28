a20d78ef633c8f243727e944d7fbe5d768f7eea7
Modular Magician <magic-modules@google.com>
2026-01-28 21:31:27 +0000

    Allow manual triggers without source configuration (#16188)
    [upstream:5ebe37eda323fef56b40cd3adc01fdd8d4a89cdf]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- cloudbuild_trigger_manual/main.tf
+++ cloudbuild_trigger_manual/main.tf
@@ -1,26 +1,17 @@
-
 resource "google_cloudbuild_trigger" "manual-trigger" {
-  name        = "manual-trigger-${local.name_suffix}"
+  name = "manual-trigger-${local.name_suffix}"
 
-  source_to_build {
-    uri       = "https://hashicorp/terraform-provider-google-beta"
-    ref       = "refs/heads/main"
-    repo_type = "GITHUB"
+  build {
+    step {
+      name = "gcr.io/cloud-builders/gcloud"
+      args = ["version"]
+    }
   }
 
-  git_file_source {
-    path      = "cloudbuild.yaml"
-    uri       = "https://hashicorp/terraform-provider-google-beta"
-    revision  = "refs/heads/main"
-    repo_type = "GITHUB"
-  }
-
-  
-  // If this is set on a build, it will become pending when it is run, 
+  // approval_config can be used with any trigger type, not just manual triggers.
+  // If this is set on a build, it will become pending when it is run,
   // and will need to be explicitly approved to start.
   approval_config {
      approval_required = true 
   }
-   
-  
 }
