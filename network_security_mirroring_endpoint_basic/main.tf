950bc3bc7fbeda5534b9e5c1459961245927ead6
The Magician <magic-modules@google.com>
2026-02-03 16:41:54 -0800

    Promote network security mirroring broker to GA. (#16286) (#1210)
    [upstream:80612a31b281604dbe9f76beb4074647b8d320f1]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_security_mirroring_endpoint_basic/main.tf
@@ -0,0 +1,27 @@
+resource "google_compute_network" "network" {
+  name                    = "example-network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_network_security_mirroring_deployment_group" "deployment_group" {
+  mirroring_deployment_group_id = "example-dg-${local.name_suffix}"
+  location                      = "global"
+  network                       = google_compute_network.network.id
+}
+
+resource "google_network_security_mirroring_endpoint_group" "endpoint_group" {
+  mirroring_endpoint_group_id = "example-eg-${local.name_suffix}"
+  location                    = "global"
+  type                        = "BROKER"
+  mirroring_deployment_groups = [google_network_security_mirroring_deployment_group.deployment_group.id]
+}
+
+resource "google_network_security_mirroring_endpoint" "default" {
+  mirroring_endpoint_id    = "example-endpoint-${local.name_suffix}"
+  location                 = "us-west2-a"
+  mirroring_endpoint_group = google_network_security_mirroring_endpoint_group.endpoint_group.id
+  description              = "some description"
+  labels = {
+    foo = "bar"
+  }
+}
