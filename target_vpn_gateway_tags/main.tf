e2185313966fccbea046f844a08ad9243ccf844e
Modular Magician <magic-modules@google.com>
2026-03-05 17:46:34 +0000

    Promote Tags to GA for VPN  (#16606)
    [upstream:de3d28023a147d1994d07711802bc4f5cea6d603]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ target_vpn_gateway_tags/main.tf
@@ -0,0 +1,76 @@
+data "google_project" "project" {
+}
+
+resource "google_compute_vpn_gateway" "target_gateway_tags" {
+  name    = "vpn-1-${local.name_suffix}"
+  network = google_compute_network.network1.id
+  params {
+    resource_manager_tags = {
+      (google_tags_tag_key.tag_key1.id) = (google_tags_tag_value.tag_value1.id)
+    }
+  }
+}
+
+resource "google_tags_tag_key" "tag_key1" {
+  parent     = "organizations/123456789-${local.name_suffix}"
+  short_name = "tagkey-${local.name_suffix}"
+}
+
+resource "google_tags_tag_value" "tag_value1" {
+  parent     = google_tags_tag_key.tag_key1.id
+  short_name = "tagvalue-${local.name_suffix}"
+}
+
+resource "google_compute_network" "network1" {
+  name = "network-1-${local.name_suffix}"
+}
+
+resource "google_compute_address" "vpn_static_ip" {
+  name = "vpn-static-ip-${local.name_suffix}"
+}
+
+resource "google_compute_forwarding_rule" "fr_esp" {
+  name        = "fr-esp-${local.name_suffix}"
+  ip_protocol = "ESP"
+  ip_address  = google_compute_address.vpn_static_ip.address
+  target      = google_compute_vpn_gateway.target_gateway_tags.id
+}
+
+resource "google_compute_forwarding_rule" "fr_udp500" {
+  name        = "fr-udp500-${local.name_suffix}"
+  ip_protocol = "UDP"
+  port_range  = "500"
+  ip_address  = google_compute_address.vpn_static_ip.address
+  target      = google_compute_vpn_gateway.target_gateway_tags.id
+}
+
+resource "google_compute_forwarding_rule" "fr_udp4500" {
+  name        = "fr-udp4500-${local.name_suffix}"
+  ip_protocol = "UDP"
+  port_range  = "4500"
+  ip_address  = google_compute_address.vpn_static_ip.address
+  target      = google_compute_vpn_gateway.target_gateway_tags.id
+}
+
+resource "google_compute_vpn_tunnel" "tunnel1" {
+  name          = "tunnel1-${local.name_suffix}"
+  peer_ip       = "15.0.0.120"
+  shared_secret = "a secret message"
+
+  target_vpn_gateway = google_compute_vpn_gateway.target_gateway_tags.id
+
+  depends_on = [
+    google_compute_forwarding_rule.fr_esp,
+    google_compute_forwarding_rule.fr_udp500,
+    google_compute_forwarding_rule.fr_udp4500,
+  ]
+}
+
+resource "google_compute_route" "route1" {
+  name       = "route1-${local.name_suffix}"
+  network    = google_compute_network.network1.name
+  dest_range = "15.0.0.0/24"
+  priority   = 1000
+
+  next_hop_vpn_tunnel = google_compute_vpn_tunnel.tunnel1.id
+}
