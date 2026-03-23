f54735cd750aa22a9e51bb1b1119a5de74f74e51
The Magician <magic-modules@google.com>
2026-03-23 11:55:53 -0700

    Fix TcpRoute tests (#16785) (#1256)
    [upstream:41243090fb726cde741a3c0de27e549ec4471f10]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- network_services_tcp_route_gateway_basic/main.tf
+++ network_services_tcp_route_gateway_basic/main.tf
@@ -1,13 +1,15 @@
 resource "google_compute_backend_service" "default" {
-  name          = "my-backend-service-${local.name_suffix}"
-  health_checks = [google_compute_http_health_check.default.id]
+  name                  = "my-backend-service-${local.name_suffix}"
+  load_balancing_scheme = "INTERNAL_SELF_MANAGED"
+  health_checks         = [google_compute_health_check.default.id]
 }
 
-resource "google_compute_http_health_check" "default" {
-  name               = "backend-service-health-check-${local.name_suffix}"
-  request_path       = "/"
-  check_interval_sec = 1
-  timeout_sec        = 1
+resource "google_compute_health_check" "default" {
+  name = "backend-service-health-check-${local.name_suffix}"
+
+  tcp_health_check {
+    port = 80
+  }
 }
 
 resource "google_network_services_gateway" "default" {
