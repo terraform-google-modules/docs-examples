5d1ee0dc932e21bcd5e3086dbd342e328a82f550
Modular Magician <magic-modules@google.com>
2026-03-26 22:16:27 +0000

    Update test for Oracle Database@Google Cloud Autonomous AI Database Disaster Recovery (#16813)
    [upstream:cf483386935d49214ebb659653540f0d66ad7624]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- oracledatabase_autonomous_database_disaster_recovery/main.tf
+++ oracledatabase_autonomous_database_disaster_recovery/main.tf
@@ -1,9 +1,27 @@
+resource "google_oracle_database_autonomous_database" "adb-dr"{
+  autonomous_database_id = "my-instance-${local.name_suffix}"
+  location = "us-east4"
+  project = "my-project-${local.name_suffix}"
+  database = "mydatabase-${local.name_suffix}"
+  admin_password = "123Abpassword"
+  properties {
+    compute_count = "2"
+    data_storage_size_gb="20"
+    db_version = "19c"
+    db_workload = "OLTP"
+    license_type = "LICENSE_INCLUDED"
+    mtls_connection_required = "true"
+    }
+  deletion_protection = "true-${local.name_suffix}"
+}
+
+
 resource "google_oracle_database_autonomous_database" "myADB"{
-  autonomous_database_id = "my-primary-adb-id-${local.name_suffix}"
+  autonomous_database_id = "my-instance-${local.name_suffix}"
   location = "my-location-${local.name_suffix}"
   project = "my-project-${local.name_suffix}"
   source_config {
-    autonomous_database = ""projects/my-project/locations/us-east4/autonomousDatabases/adb-id"-${local.name_suffix}"
+    autonomous_database = google_oracle_database_autonomous_database.adb-dr.name
     automatic_backups_replication_enabled = "false-${local.name_suffix}"
     }
   deletion_protection = "true-${local.name_suffix}"
