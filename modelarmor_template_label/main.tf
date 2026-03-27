f7b16c1362d4d6d7ce3f36adb12d413c8831f51c
The Magician <magic-modules@google.com>
2026-03-27 15:02:12 -0700

    Update Test Generation: Update example template (#16703) (#1263)
    [upstream:067d044403be779be5fba41b59cce17483c6801a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- modelarmor_template_label/main.tf
+++ modelarmor_template_label/main.tf
@@ -1,28 +1,28 @@
 resource "google_model_armor_template" "template-label-advanced-config" {
-  location    = "<no value>"
-  template_id = "<no value>"
+  location    = "us-central1-${local.name_suffix}"
+  template_id = "modelarmor4-${local.name_suffix}"
 
   labels = {
-    "test-label" = "<no value>"
+    "test-label" = "template-test-label-${local.name_suffix}"
   }
 
   filter_config {
     rai_settings {
       rai_filters {
-        filter_type      = "<no value>"
-        confidence_level = "<no value>"
+        filter_type      = "DANGEROUS-${local.name_suffix}"
+        confidence_level = "MEDIUM_AND_ABOVE-${local.name_suffix}"
       }
     }
     sdp_settings {
       advanced_config {
-        inspect_template     = "<no value>"
-        deidentify_template  = "<no value>"
+        inspect_template     = "projects/llm-firewall-demo/locations/us-central1/inspectTemplates/t3-${local.name_suffix}"
+        deidentify_template  = "projects/llm-firewall-demo/locations/us-central1/deidentifyTemplates/t2-${local.name_suffix}"
       }
     }
   }
   template_metadata {
     multi_language_detection {
-      enable_multi_language_detection = <no value>
+      enable_multi_language_detection = false-${local.name_suffix}
     }
   }
 }
--- modelarmor_template_template_metadata/main.tf
+++ modelarmor_template_template_metadata/main.tf
@@ -1,26 +1,26 @@
 resource "google_model_armor_template" "template-template-metadata" {
-  location    = "<no value>"
-  template_id = "<no value>"
+  location    = "us-central1-${local.name_suffix}"
+  template_id = "modelarmor3-${local.name_suffix}"
 
   filter_config {
     rai_settings {
       rai_filters {
-        filter_type      = "<no value>"
-        confidence_level = "<no value>"
+        filter_type      = "HARASSMENT-${local.name_suffix}"
+        confidence_level = "MEDIUM_AND_ABOVE-${local.name_suffix}"
       }
     }
   }
   template_metadata {
-    custom_llm_response_safety_error_message = "<no value>"
-    log_sanitize_operations                  = <no value>
-    log_template_operations                  = <no value>
+    custom_llm_response_safety_error_message = "This is a custom error message for LLM response-${local.name_suffix}"
+    log_sanitize_operations                  = false-${local.name_suffix}
+    log_template_operations                  = true-${local.name_suffix}
     multi_language_detection {
-      enable_multi_language_detection        = <no value>
+      enable_multi_language_detection        = true-${local.name_suffix}
     }
-    ignore_partial_invocation_failures       = <no value>
-    custom_prompt_safety_error_code          = <no value>
-    custom_prompt_safety_error_message       = "<no value>"
-    custom_llm_response_safety_error_code    = <no value>
-    enforcement_type                         = "<no value>"
+    ignore_partial_invocation_failures       = false-${local.name_suffix}
+    custom_prompt_safety_error_code          = 400-${local.name_suffix}
+    custom_prompt_safety_error_message       = "This is a custom error message for prompt-${local.name_suffix}"
+    custom_llm_response_safety_error_code    = 401-${local.name_suffix}
+    enforcement_type                         = "INSPECT_ONLY-${local.name_suffix}"
   }
 }
