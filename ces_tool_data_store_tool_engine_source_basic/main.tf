a2d45b77954a2de3d7be3dc10112f874f1ace1b0
The Magician <magic-modules@google.com>
2026-04-23 12:11:51 -0700

    Update Test Generation: Apply iam test changes to sample generator (#17252) (#1298)
    [upstream:791d94c0a2cb729b197c33e73f07bc7861ef116d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- ces_tool_data_store_tool_engine_source_basic/main.tf
+++ ces_tool_data_store_tool_engine_source_basic/main.tf
@@ -56,7 +56,7 @@ resource "google_ces_tool" "ces_tool_data_store_tool_engine_source_basic" {
             modality_type = "TEXT"
             rewriter_config {
                 model_settings {
-                    model = "gemini-2.5-flash"
+                    model = "gemini-2.5-flash-001"
                     temperature = 1
                 }
                 prompt = "example-prompt"
@@ -64,7 +64,7 @@ resource "google_ces_tool" "ces_tool_data_store_tool_engine_source_basic" {
             }
             summarization_config {
                 model_settings {
-                    model = "gemini-2.5-flash"
+                    model = "gemini-2.5-flash-001"
                     temperature = 1
                 }
                 prompt = "example-prompt"
