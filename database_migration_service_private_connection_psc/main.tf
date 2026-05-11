e5163a67a26848f5653732f4670c01ee040b3db5
The Magician <magic-modules@google.com>
2026-05-11 09:32:19 -0700

    Add PSC connectivity support to Database Migration Service PrivateConnection (#17439) (#1312)
    [upstream:1f6c3df3ea67d2f1ba850b09462d8e57ecc7b2c9]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ database_migration_service_private_connection_psc/main.tf
@@ -0,0 +1,34 @@
+resource "google_database_migration_service_private_connection" "default" {
+	display_name          = "dbms_pc"
+	location              = "us-central1"
+	private_connection_id = "my-connection-${local.name_suffix}"
+
+	labels = {
+		key = "value"
+	}
+
+	psc_interface_config {
+		network_attachment = resource.google_compute_network_attachment.default.id
+	}
+
+	create_without_validation = false
+}
+
+resource "google_compute_network_attachment" "default" {
+  name                  = "my-attachment-${local.name_suffix}"
+  region                = "us-central1"
+  connection_preference = "ACCEPT_AUTOMATIC"
+  subnetworks           = [resource.google_compute_subnetwork.default.id]
+}
+
+resource "google_compute_network" "default" {
+  name = "my-network-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-subnetwork-${local.name_suffix}"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
