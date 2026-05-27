dedc6aadafbc54f79846cad837a98c69d2584455
The Magician <magic-modules@google.com>
2026-05-27 13:03:04 -0700

    Add migration center discovery client resource and tests (#17581) (#1329)
    [upstream:a6c938bd12236677f43f6ecdbae6bf6590dc1923]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ migration_center_discovery_client_update/main.tf
@@ -0,0 +1,29 @@
+resource "google_migration_center_source" "default" {
+  location  = "us-central1"
+  source_id = "source-test-${local.name_suffix}"
+  type      = "SOURCE_TYPE_DISCOVERY_CLIENT"
+}
+
+resource "google_service_account" "default" {
+  account_id   = "sa-test-${local.name_suffix}"
+  display_name = "Service Account for Discovery Client"
+}
+
+resource "google_service_account" "default_2" {
+  account_id   = "sa-test-two-${local.name_suffix}"
+  display_name = "Second Service Account for Discovery Client"
+}
+
+resource "google_migration_center_discovery_client" "default" {
+  location            = "us-central1"
+  discovery_client_id = "discovery-client-test-${local.name_suffix}"
+  source              = google_migration_center_source.default.id
+  service_account     = google_service_account.default_2.email
+  display_name        = "Terraform integration test display updated"
+  description         = "Terraform integration test description updated"
+  expire_time         = "2030-01-02T00:00:00Z"
+  labels = {
+    my_key     = "value_updated"
+    second_key = "second_value"
+  }
+}
