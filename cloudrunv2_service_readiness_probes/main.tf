e0bd2205e22163c84930b37e1df9900ce5f4b95d
The Magician <magic-modules@google.com>
2026-06-11 14:42:33 -0700

    Remove launchStage from readiness_probe examples (#17925) (#1344)
    [upstream:b4b89994ffc5bb93f09f794138a417ce25385eac]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- cloudrunv2_service_readiness_probes/main.tf
+++ cloudrunv2_service_readiness_probes/main.tf
@@ -2,7 +2,6 @@ resource "google_cloud_run_v2_service" "default" {
   name     = "cloudrun-service-${local.name_suffix}"
   location = "us-central1"
   deletion_protection = false
-  launch_stage = "BETA"
 
   template {
     containers {
