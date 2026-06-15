21d7b6854cff25ce56392f41d1bd4bb2dfccde9a
Modular Magician <magic-modules@google.com>
2026-06-15 20:41:57 +0000

    feat: Add sort_order support to BigLake Iceberg Table (#17919)
    [upstream:2d572fff6923787834faefd86534953c92d8749e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ biglake_iceberg_table_sort_order/main.tf
@@ -0,0 +1,39 @@
+resource "google_storage_bucket" "bucket" {
+  name          = "my-bucket-${local.name_suffix}"
+  location      = "us-central1"
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_biglake_iceberg_catalog" "catalog" {
+  name = google_storage_bucket.bucket.name
+  catalog_type = "CATALOG_TYPE_GCS_BUCKET"
+}
+
+resource "google_biglake_iceberg_namespace" "namespace" {
+  catalog = google_biglake_iceberg_catalog.catalog.name
+  namespace_id = "my_namespace-${local.name_suffix}"
+}
+
+resource "google_biglake_iceberg_table" "my_iceberg_table" {
+  catalog   = google_biglake_iceberg_catalog.catalog.name
+  namespace = google_biglake_iceberg_namespace.namespace.namespace_id
+  name      = "my_table-${local.name_suffix}"
+  schema {
+    type = "struct"
+    fields {
+      id       = 1
+      name     = "id"
+      type     = "long"
+      required = true
+    }
+  }
+  sort_order {
+    fields {
+      source_id  = 1
+      transform  = "identity"
+      direction  = "asc"
+      null_order = "nulls-first"
+    }
+  }
+}
