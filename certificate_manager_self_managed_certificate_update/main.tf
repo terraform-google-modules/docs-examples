24cb5d7f2ab1cc629e5d5c49d7a8ec4917ee86be
Modular Magician <magic-modules@google.com>
2026-07-17 17:18:08 +0000

    Feature branch major release 8.0.0 main sync july 17 (#18316)
    [upstream:f878a596a21727d5c9bd64a400c00a8894885e1a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ certificate_manager_self_managed_certificate_update/main.tf
@@ -0,0 +1,9 @@
+resource "google_certificate_manager_certificate" "default" {
+  name        = "self-managed-cert-${local.name_suffix}"
+  description = "Global cert"
+  scope       = "ALL_REGIONS"
+  self_managed {
+    pem_certificate = file("test-fixtures/cert-updated.pem")
+    pem_private_key = file("test-fixtures/private-key-updated.pem")
+  }
+}
--- region_target_tcp_proxy_basic/main.tf
+++ region_target_tcp_proxy_basic/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_region_target_tcp_proxy" "default" {
   name            = "test-proxy-${local.name_suffix}"
-  region          = "europe-west4"
+  region          = "us-central1"
   backend_service = google_compute_region_backend_service.default.id
 }
 
@@ -8,7 +8,7 @@ resource "google_compute_region_backend_service" "default" {
   name        = "backend-service-${local.name_suffix}"
   protocol    = "TCP"
   timeout_sec = 10
-  region      = "europe-west4"
+  region      = "us-central1"
 
   health_checks         = [google_compute_region_health_check.default.id]
   load_balancing_scheme = "INTERNAL_MANAGED"
@@ -16,7 +16,7 @@ resource "google_compute_region_backend_service" "default" {
 
 resource "google_compute_region_health_check" "default" {
   name               = "health-check-${local.name_suffix}"
-  region             = "europe-west4"
+  region             = "us-central1"
   timeout_sec        = 1
   check_interval_sec = 1
   tcp_health_check {
