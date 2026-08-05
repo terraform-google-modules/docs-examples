1f99929b8d7ca39447e1e0e65ef82444bf22c55d
The Magician <magic-modules@google.com>
2026-08-05 09:52:13 -0700

    Securesourcemanager byosa scanning (#18240) (#1390)
    [upstream:f1617ff7cba6dd35c0c67aff173762cb18f62978]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secure_source_manager_repository_secret_scanning_default/main.tf
@@ -0,0 +1,18 @@
+resource "google_secure_source_manager_instance" "instance" {
+  location = "us-central1"
+  instance_id = "my-instance-${local.name_suffix}"
+  deletion_policy = ""DELETE""
+}
+
+resource "google_secure_source_manager_repository" "default" {
+  location = "us-central1"
+  repository_id = "my-repository-${local.name_suffix}"
+  instance = google_secure_source_manager_instance.instance.name
+  deletion_policy = ""DELETE""
+
+  scan_config {
+    secret_scan_config {
+      enabled = true
+    }
+  }
+}
