88a31af4c022ffa3c5d43a4157b7d22c5cd4d303
The Magician <magic-modules@google.com>
2026-08-20 14:52:57 -0700

    Merge feature branch major release 8.0.0 (#18733) (#1405)
    [upstream:36aa11a68f0737f1eda4b302c9f8321ed44494bf]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- backend_service_cache_simple/main.tf
+++ backend_service_cache_simple/main.tf
@@ -1,15 +1,18 @@
 resource "google_compute_backend_service" "default" {
   name          = "backend-service-${local.name_suffix}"
-  health_checks = [google_compute_http_health_check.default.id]
+  health_checks = [google_compute_health_check.default.id]
   enable_cdn  = true
   cdn_policy {
     signed_url_cache_max_age_sec = 7200
   }
 }
 
-resource "google_compute_http_health_check" "default" {
+resource "google_compute_health_check" "default" {
   name               = "health-check-${local.name_suffix}"
-  request_path       = "/"
   check_interval_sec = 1
   timeout_sec        = 1
+  http_health_check {
+    port         = 80
+    request_path = "/"
+  }
 }
