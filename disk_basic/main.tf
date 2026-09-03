37f4ffcf210f6fb2221c18be1999d3466732d851
The Magician <magic-modules@google.com>
2026-09-02 18:06:20 -0700

    tests: update debian-11 compute images and families to debian-13 (#18857) (#1417)
    [upstream:dc7a93753acaea02bf94c91bfb64fb80cfc2c8b5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- disk_basic/main.tf
+++ disk_basic/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_disk" "default" {
   name  = "test-disk-${local.name_suffix}"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  image = "debian-11-bullseye-v20220719"
+  image = "debian-13-trixie-v20260827"
   labels = {
     environment = "dev"
   }
