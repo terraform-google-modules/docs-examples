5d1711a9072c61db6ca91b7c95a884b9ff26cafa
The Magician <magic-modules@google.com>
2026-09-17 09:52:22 -0700

    vmwareengine: make sample network names sweepable (#19042) (#1447)
    [upstream:7db2322c899e3bf83ca2a5f474cbf9c403ae6fed]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- vmware_engine_network_standard/main.tf
+++ vmware_engine_network_standard/main.tf
@@ -1,5 +1,5 @@
 resource "google_vmwareengine_network" "vmw-engine-network" {
-    name              = "standard-nw"
+    name              = "standard-nw-${local.name_suffix}"
     location          = "global" # Standard network needs to be global
     type              = "STANDARD"
     description       = "VMwareEngine standard network sample"
