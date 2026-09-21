25b04e904d09ae6327798af034f637cf8a05260c
Modular Magician <magic-modules@google.com>
2026-09-21 21:36:43 +0000

    compute: add `rsa_encryption_wo` and `raw_key_wo` fields to `google_compute_disk` and `google_compute_region_disk` resources (#18851)
    [upstream:084968a972920a45a77a64a94055d8ff03e23904]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- disk_basic_wo/main.tf
+++ disk_basic_wo/main.tf
@@ -2,13 +2,13 @@ resource "google_compute_disk" "default" {
   name  = "test-disk-${local.name_suffix}"
   type  = "pd-ssd"
   zone  = "us-central1-a"
-  image = "debian-11-bullseye-v20220719"
+  image = "debian-13-trixie-v20260827"
   labels = {
     environment = "dev"
   }
+  physical_block_size_bytes = 4096
   disk_encryption_key {
-    raw_key_wo = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
+    raw_key_wo         = "SGVsbG8gZnJvbSBHb29nbGUgQ2xvdWQgUGxhdGZvcm0="
     raw_key_wo_version = 1
   }
-  physical_block_size_bytes = 4096
 }
