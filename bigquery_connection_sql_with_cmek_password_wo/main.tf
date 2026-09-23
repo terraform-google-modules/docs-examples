6e696052d029e88fffbf884ec66efdcff971cf3d
The Magician <magic-modules@google.com>
2026-09-23 09:39:14 -0700

    bigqueryconnection: added `password_wo` write-only field in `google_bigquery_connection` resource (#13541) (#1462)
    [upstream:3ab7d65434de9807d3ec479ea0ff029f3a2a6fd2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_connection_sql_with_cmek_password_wo/main.tf
@@ -0,0 +1,39 @@
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance-${local.name_suffix}"
+  region           = "us-central1"
+
+  database_version = "POSTGRES_11"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = false
+}
+
+resource "google_sql_database" "db" {
+  instance = google_sql_database_instance.instance.name
+  name     = "db"
+}
+
+resource "google_sql_user" "user" {
+  name = "user-${local.name_suffix}"
+  instance = google_sql_database_instance.instance.name
+  password = "tf-test-my-password%{random_suffix}"
+}
+
+resource "google_bigquery_connection" "bq-connection-cmek" {
+  friendly_name = "👋"
+  description   = "a riveting description"
+  location      = "US"
+  kms_key_name  = "projects/project/locations/us-central1/keyRings/us-central1/cryptoKeys/bq-key-${local.name_suffix}"
+  cloud_sql {
+    instance_id = google_sql_database_instance.instance.connection_name
+    database    = google_sql_database.db.name
+    type        = "POSTGRES"
+    credential {
+      username             = google_sql_user.user.name
+      password_wo          = google_sql_user.user.password
+      password_wo_version  = "1"
+    }
+  }
+}
