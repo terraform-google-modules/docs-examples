fb57b5e22e9cb5dcf119ec6405fc6f97d237b2bd
Modular Magician <magic-modules@google.com>
2026-09-25 00:34:07 +0000

    parametermanager: add data_crc32c to google_parameter_manager_parameter_version and regional (#18969)
    [upstream:410fe7f78ac35a7157b5420d4295259c1f564aab]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ regional_parameter_version_with_data_crc32c/main.tf
@@ -0,0 +1,11 @@
+resource "google_parameter_manager_regional_parameter" "regional-parameter-basic" {
+  parameter_id = "regional_parameter-${local.name_suffix}"
+  location = "us-central1"
+}
+
+resource "google_parameter_manager_regional_parameter_version" "regional-parameter-version-with-data-crc32c" {
+  parameter = google_parameter_manager_regional_parameter.regional-parameter-basic.id
+  parameter_version_id = "regional_parameter_version-${local.name_suffix}"
+  parameter_data = "regional-parameter-version-data"
+  data_crc32c = "4019737965"
+}
